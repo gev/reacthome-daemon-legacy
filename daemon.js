4dc9a584d3e95d2b8137ace16410c4181c404466
Evgeny <gazdovsky@gmail.com>
2018-10-25 01:04:20 +0300

    koa

--- daemon.js
+++ daemon.js
@@ -1,9 +1,11 @@
 
+const Koa = require('koa');
+const static = require('koa-static');
 const { assets, device, service } = require('./src/controllers');
 const createStore = require('./src/store');
 const reducer = require('./src/reducer');
 const { set } = require('./src/actions');
-const { mac, DAEMON, SERVICE_PORT, ACTION_SET } = require('./src/constants');
+const { mac, DAEMON, SERVICE_PORT, ACTION_SET, IMAGE } = require('./src/constants');
 const db = require('./src/db');
 
 const state = {};
@@ -16,8 +18,16 @@ db.createReadStream()
     state[key] = value;
   })
   .on('end', () => {
+    const app = new Koa();
     const store = createStore(reducer, state);
     store.dispatch(set(mac, { type: DAEMON }));
+    app.use(static('./tmp/assets/'));
+    app.use(async (ctx) => {
+      await ctx.text();
+      console.log(cts.request);
+    });
+    app.listen(SERVICE_PORT);
     service.manage(store);
     device.manage(store);
   })
+
