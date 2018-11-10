1bd1e4297ef115ca0b13455be96f79a5834337d7
Evgeny <gazdovsky@gmail.com>
2018-11-10 15:00:28 +0300

    up

--- src/actions/create.js
+++ src/actions/create.js
@@ -9,7 +9,7 @@ module.exports.get = state.get;
 
 const apply = (id, payload) => {
   state.set(id, payload);
-  service.broadcast(JSON.stringify(action));
+  service.broadcast(JSON.stringify({ type: ACTION_SET, id, payload }));
   db.put(id, get(id), (err) => {
     if (err) console.log(err);
   });
