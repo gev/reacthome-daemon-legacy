e440a2f3b9567814983586c3b363880702aecf83
Evgeny <gazdovsky@gmail.com>
2018-12-08 00:13:31 +0300

    up

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -125,14 +125,14 @@ const run = (action, address) => {
       }
       case ACTION_DISCOVERY: {
         const { id, payload } = action;
-        const { multicast, type, version } = payload;
+        const { multicast, type, version, ready } = payload;
         if (multicast) {
           service.delUnicast(address);
         } else {
           service.addUnicast(address);
         }
         if (id && type !== MOBILE) {
-          set(id, { online: true, ip: address, multicast, version });
+          set(id, { online: true, ip: address, type, multicast, version, ready });
           add(mac, DEVICE, id);
           clearTimeout(timer[id]);
           timer[id] = setTimeout(() => {
