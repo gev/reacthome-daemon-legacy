cf7e56dde586965e0cf3f6f1ec71c03fed6a3e95
Evgeny <gazdovsky@gmail.com>
2018-12-22 16:37:01 +0300

    up

---
+++ src/actions/count.js
@@ -0,0 +1,21 @@
+
+const { get, set } = require('./create');
+
+const count_on = (site, type) => {
+  const { count = {}, parent } = get(site);
+  const n = count[type];
+  set(site, { count: { ...count, [type]: (Number.isInteger(n) && n) < 0 ? 0 : n + 1 } });
+  if (parent) count_on(parent, type);
+};
+
+module.exports.count_on = count_on;
+
+const count_off = (site, type) => {
+  const { count = {}, parent } = get(site);
+  const n = count[type];
+  set(site, { count: { ...count, [type]: (Number.isInteger(n) && n) > 0 ? n - 1 : 0 } });
+  if (parent) count_off(parent, type);
+};
+
+module.exports.count_off = count_off;
+
