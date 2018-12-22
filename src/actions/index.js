485bc8418019781e77bc6c367e206f59f6e24e6f
Evgeny <gazdovsky@gmail.com>
2018-12-22 20:13:46 +0300

    up

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,12 +3,12 @@ const { get, set, add, apply, applySite } = require('./create');
 const { online, offline } = require('./status');
 const { pendingFirmware, updateFirmware } = require('./firmware');
 const { initialize, initialized } = require('./init');
-const { count_on: count_on, count_off } = require('./count');
+const { count, count_on, count_off } = require('./count');
 
 module.exports = {
   get, set, add, apply, applySite,
   online, offline,
   pendingFirmware, updateFirmware,
   initialize, initialized,
-  count_on, count_off,
+  count, count_on, count_off,
 };
