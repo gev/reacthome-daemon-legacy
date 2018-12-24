e91bb1c04e65f24a34fffaeb693445ad5888acce
Evgeny <gazdovsky@gmail.com>
2018-12-24 21:50:39 +0300

    ???

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -19,7 +19,7 @@ module.exports = (discovery, interval, port, listen) => {
         socket.send(q.shift(), port, ip, (err) => {
           if (err) console.error(error);
         });
-      }, 100);
+      }, 10);
     }
     queue[ip].push(packet);
     console.log('push', ip, queue[ip].length);
