e418abe7f3e746a9b24e382e60e4ef103cd8594f
Evgeny <gazdovsky@gmail.com>
2018-12-24 22:38:40 +0300

    up

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -21,7 +21,7 @@ module.exports = (discovery, interval, port, listen, hasQueue) => {
           });
         } else {
           console.log('waiting', ip);
-          setTimeout(send, 30, packet, ip);
+          setTimeout(send, 20 - now + ts, packet, ip);
         }
       } else {
         timestamp[ip] = Date.now();
