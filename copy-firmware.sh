a36ed3875dbbfe9c219286197a2070579f6566b9
Evgeny <gazdovsky@gmail.com>
2019-02-22 21:03:42 +0300

    up pnp firmware

--- copy-firmware.sh
+++ copy-firmware.sh
@@ -1,7 +1,7 @@
 
-list=`find ../../pic-sensor.X/dist -type f -name pic-sensor.X.production.hex`
+list=`find ../pic-sensor.X/dist -type f -name pic-sensor.X.production.hex`
 for i in $list
 do
-  name="$(cut -d'/' -f5 <<< $i)"
+  name="$(cut -d'/' -f4 <<< $i)"
   cp $i "./firmware/$name.hex"
 done
