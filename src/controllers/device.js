227c2490dccc97143fccc023405e8949e8c932cb
Evgeny <gazdovsky@gmail.com>
2019-02-26 22:30:46 +0300

    rollback

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -171,7 +171,7 @@ module.exports.manage = () => {
           break;
         }
         case ACTION_ARTNET: {
-          switch (data.length === 10) {
+          switch (data[7]) {
             case ARTNET_SIZE: {
               const size = (data[8] << 8) | data[9];
               set(id, { size });
