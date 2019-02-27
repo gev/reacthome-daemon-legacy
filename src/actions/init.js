74d7c53637185773d74134ba0d5349537bb0896d
Evgeny <gazdovsky@gmail.com>
2019-02-27 20:26:11 +0300

    artnet initialize

--- src/actions/init.js
+++ src/actions/init.js
@@ -12,6 +12,7 @@ const {
   DEVICE_TYPE_DI24,
   DEVICE_TYPE_DIM4,
   DEVICE_TYPE_DIM8,
+  DEVICE_TYPE_ARTNET,
   DEVICE_TYPE_SENSOR4,
   DEVICE_TYPE_PLC,
   DISCOVERY_INTERVAL
@@ -31,28 +32,28 @@ module.exports.initialize = (id) => {
   switch (dev.type) {
     case DEVICE_TYPE_SENSOR4: {
       for (let i = 1; i <= 4; i++) {
-        const channel = get(`${id}/${DI}/${i}`); 
+        const channel = get(`${id}/${DI}/${i}`);
         a[i] = (channel && channel.value) || 0;
       }
       break;
     }
     case DEVICE_TYPE_DI24: {
       for (let i = 1; i <= 24; i++) {
-        const channel = get(`${id}/${DI}/${i}`); 
+        const channel = get(`${id}/${DI}/${i}`);
         a[i] = (channel && channel.value) || 0;
       }
       break;
     }
     case DEVICE_TYPE_DO8: {
       for (let i = 1; i <= 8; i++) {
-        const channel = get(`${id}/${DO}/${i}`); 
+        const channel = get(`${id}/${DO}/${i}`);
         a[i] = (channel && channel.value) || 0;
       }
       break;
     }
     case DEVICE_TYPE_DO12: {
       for (let i = 1; i <= 12; i++) {
-        const channel = get(`${id}/${DO}/${i}`); 
+        const channel = get(`${id}/${DO}/${i}`);
         a[i] = (channel && channel.value) || 0;
       }
       break;
@@ -67,7 +68,7 @@ module.exports.initialize = (id) => {
     }
     case DEVICE_TYPE_DIM8: {
       for (let i = 1; i <= 8; i++) {
-        const channel = get(`${id}/${DIM}/${i}`); 
+        const channel = get(`${id}/${DIM}/${i}`);
         a[2 * i - 1] = (channel && channel.type) || 0;
         a[2 * i] = (channel && channel.value) || 0;
       }
@@ -75,18 +76,30 @@ module.exports.initialize = (id) => {
     }
     case DEVICE_TYPE_PLC: {
       for (let i = 1; i <= 36; i++) {
-        const channel = get(`${id}/${DI}/${i}`); 
+        const channel = get(`${id}/${DI}/${i}`);
         a[i] = (channel && channel.value) || 0;
       }
       for (let i = 1; i <= 24; i++) {
-        const channel = get(`${id}/${DO}/${i}`); 
+        const channel = get(`${id}/${DO}/${i}`);
         a[i + 36] = (channel && channel.value) || 0;
       }
       break;
     }
-    default:
+    case DEVICE_TYPE_ARTNET: {
+      const dev = get(id);
+      const { host, port, net, subnet, universe, rate, size } = dev;
+      const config = { host, port, net, subnet, universe, rate, size, ...action.payload };
+      device.send(Buffer.concat([
+        Buffer.from([ACTION_ARTNET, action.action]),
+        Buffer.from(JSON.stringify(config))
+      ]), dev.ip);
+      return;
+      // break;
+    }
+    default: {
       set(id, { initialized: true });
       return;
+    }
   }
   device.sendConfirm(Buffer.from(a), dev.ip, () => {
     const d = get(id);
