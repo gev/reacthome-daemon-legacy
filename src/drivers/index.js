e862e945c82297a3677996fe6111db27438034f7
Evgeny <gazdovsky@gmail.com>
2019-04-24 13:12:37 +0300

    more debug

--- src/drivers/index.js
+++ src/drivers/index.js
@@ -18,6 +18,7 @@ module.exports.manage = () => {
   run = {};
   driver.forEach(id => {
     const { type } = get(id) || {};
+    console.log(id, type);
     switch(type) {
       case DRIVER_TYPE_RS21:
         run[id] = new RS21(id);
