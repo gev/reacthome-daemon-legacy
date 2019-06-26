c9362b1dfa35754cfee28d0758710295b489ac84
Evgeny <gazdovsky@gmail.com>
2019-06-26 15:50:42 +0300

    debug

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -209,8 +209,11 @@ const run = (action, address) => {
           case DEVICE_TYPE_RELAY_6:
           case DEVICE_TYPE_RELAY_12:
           case DEVICE_TYPE_RELAY_24: {
-            console.log(Buffer.from([ACTION_DO, ...(action.id.split(':').map(i => parseInt(16))), action.index, action.value]));
-            device.send(Buffer.from([ACTION_DO, ...(action.id.split(':').map(i => parseInt(16))), action.index, action.value]), dev.ip);
+            console.log(action.id);
+            console.log(action.id.split(':'));
+            console.log(action.id.split(':').map(i => parseInt(16));
+            console.log(Buffer.from([ACTION_DO, ...action.id.split(':').map(i => parseInt(16)), action.index, action.value]))
+            device.send(Buffer.from([ACTION_DO, ...action.id.split(':').map(i => parseInt(16)), action.index, action.value]), dev.ip);
             break;
           }
           default: {
