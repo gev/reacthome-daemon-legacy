71d84c2011770d8cfd774486f2f5bd5881af5d09
Evgeny <gazdovsky@gmail.com>
2019-07-22 21:37:55 +0300

    up

--- src/drivers/M206/index.js
+++ src/drivers/M206/index.js
@@ -12,7 +12,7 @@ const period = 15000;
 const cmd = [0x27, 0x85, 0x63, 0x81];
 
 const number = x =>
-  x.reduce((a, b) => 100 * a + (10 * (b >> 4 & 0xf) + (b & 0xf)), 0);
+  x.reduce((a, b) => (100 * a) + (10 * (b >> 4 & 0xf) + (b & 0xf)), 0);
 
 module.exports = class {
 
