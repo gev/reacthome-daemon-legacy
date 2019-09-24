5dcce9cb1e397f5783d82c94c0958f9924db83fc
Evgeny <gazdovsky@gmail.com>
2019-09-24 14:04:33 +0300

    up

--- src/webrtc/peer.js
+++ src/webrtc/peer.js
@@ -3,12 +3,12 @@ const peers = new Map();
 const channels = new Map();
 
 const send = (session, message) => {
+  console.log(session, message);
   channels.get(session).send(message);
 };
 
 const broadcast = (message) => {
   channels.forEach((channel, session) => {
-    console.log(session, message);
     channel.send(message);
   });
 };
