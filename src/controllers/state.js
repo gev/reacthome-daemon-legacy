69af1c229619147bfe1ae62fae275a3371036f28
Evgeny <gazdovsky@gmail.com>
2019-09-29 21:19:07 +0300

    add timestamp

--- src/controllers/state.js
+++ src/controllers/state.js
@@ -6,7 +6,7 @@ module.exports.init = (s) => state = s;
 module.exports.get = (id) => state[id];
 
 module.exports.set = (id, payload) => {
-  state[id] = { ...state[id], ...payload };
+  state[id] = { ...state[id], ...payload, timestamp: Date.now() };
 };
 
 module.exports.state = () => state;
