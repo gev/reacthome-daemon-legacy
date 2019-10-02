e2143deec4da67a27b3ea24b6783be1ca5794be3
Evgeny <gazdovsky@gmail.com>
2019-10-02 18:16:07 +0300

    typo

--- src/janus/index.js
+++ src/janus/index.js
@@ -4,17 +4,17 @@ const { CREATE, ATTACH, MESSAGE, TRICKLE } =require('./constants');
 
 module.exports.start = connect;
 
-module.export.createSession = async () => {
+module.exports.createSession = async () => {
   const { data } = await send({ janus: CREATE });
   return data.id;
 };
 
-module.export.attachPlugin = async (session_id, plugin) => {
+module.exports.attachPlugin = async (session_id, plugin) => {
   const { data } = await send({ janus: ATTACH, session_id, plugin });
   return data.id;
 };
 
-module.export.sendMessage = (session_id, handle_id, body, jsep) =>
+module.exports.sendMessage = (session_id, handle_id, body, jsep) =>
   send({ janus: MESSAGE, session_id, body, jsep });
 
 module.exports.trickle = ({ session_id, handle_id, candidate }) =>
