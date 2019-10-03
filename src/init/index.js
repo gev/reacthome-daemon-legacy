6f1780d629931c6544c741cbe5dbf459864a28fa
Evgeny <gazdovsky@gmail.com>
2019-10-03 23:38:11 +0300

    fix init

--- src/init/index.js
+++ src/init/index.js
@@ -1,12 +1,13 @@
 
 const { state } = require('../controllers/state');
+const { sendAction } = require('../webrtc/peer');
 
 module.exports = (action, session) => {
   const timestamp = action.timestamp || 0;
   Object
     .entries(state())
     .filter(([id, payload]) =>
-      payload && !payload instanceof Array && payload instanceof Object && payload.timestamp > timestamp)
+      payload && !(payload instanceof Array) && (payload instanceof Object) && (payload.timestamp > timestamp))
     .forEach(([id, payload], i, { length }) => {
       sendAction(session, { type: 'init', id, payload, current: i + 1, total: length });
     });
