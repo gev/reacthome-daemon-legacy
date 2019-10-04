da5d91c07e03d1af9e1fb48c04fc0da507f8c1c2
Evgeny <gazdovsky@gmail.com>
2019-10-04 19:43:52 +0300

    up

--- src/util.js
+++ src/util.js
@@ -4,16 +4,14 @@ const SDP = require('sdp-transform');
 module.exports.isUUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 module.exports.fixSDP = (sdp) => {
-  console.log(sdp);
   const o = SDP.parse(sdp);
   for (let media of o.media) {
     if (media.type === 'video') {
       media.fmtp = [{
         payload: 96,
-        config: 'profile-level-id=42e01f;packetization-mode=1'
+        config: 'level-asymmetry-allowed=1;profile-level-id=42e01f;packetization-mode=1'
       }];
     }
   }
-  console.log(SDP.write(o));
   return SDP.write(o);
 }
