d99133e5c85aaf02a317aa65a6b05b0ec9a1c501
Evgeny <gazdovsky@gmail.com>
2019-10-05 21:53:54 +0300

    up

--- src/sip/handle.js
+++ src/sip/handle.js
@@ -20,6 +20,7 @@ module.exports.onRegister = (request) => {
     rs = sip.makeResponse(request, 200, 'Ok');
     rs.headers.contact = request.headers.contact;
     rs.headers.to.params.tag = uuid();
+    rs.headers.to.params.expires = 3600;
   // } else {
   //   rs = digest.challenge({ realm }, sip.makeResponse(request, 401, 'Authentication Required'));
   // }
