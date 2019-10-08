22eec1597464f3dc6d1100b3fefbbd8638e2b811
Evgeny <gazdovsky@gmail.com>
2019-10-08 23:55:39 +0300

    up

--- src/init/get.js
+++ src/init/get.js
@@ -2,15 +2,14 @@
 const { get } = require('../actions');
 const { sendAction } = require('../webrtc/peer');
 const { ACTION_SET } = require('../constants');
+const sendAsset = require('../assets/send');
 
-
-const sendAsset = (name) => {
-
-}
 
 module.exports = ({ state, assets }, session) => {
   state.forEach(id => {
     sendAction({ type: ACTION_SET, id,  payload: get(id) });
   });
-  assets.forEach(sendAsset(session))
+  assets.forEach(asset => {
+    sendAsset(session, asset);
+  });
 };
