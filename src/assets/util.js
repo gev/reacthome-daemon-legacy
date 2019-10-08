9fd37332b971139babe3feeb55811e92b31c0551
Evgeny <gazdovsky@gmail.com>
2019-10-08 19:09:13 +0300

    up

--- src/assets/util.js
+++ src/assets/util.js
@@ -1,14 +1,14 @@
 
+const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
-const { mkdir, readdir, stat, exists } = require('fs');
 const { ASSETS, TMP } = require('./constants');
 
 const p = (type) => (a = '') => path.join(type, a);
 
 module.exports.tmp = p(TMP);
 module.exports.asset = p(ASSETS);
-module.exports.stat = promisify(stat);
-module.exports.mkdir = promisify(mkdir);
-module.exports.readdir = promisify(readdir);
-module.exports.exists = file => new Promise(resolve => exists(file, resolve));
+module.exports.stat = promisify(fs.stat);
+module.exports.mkdir = promisify(fs.mkdir);
+module.exports.readdir = promisify(fs.readdir);
+module.exports.exists = file => new Promise(resolve => fs.exists(file, resolve));
