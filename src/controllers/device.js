c767e00066180e289147958ef591f060d5067a12
Evgeny <gazdovsky@gmail.com>
2019-10-09 12:46:35 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -229,7 +229,7 @@ module.exports.manage = () => {
           break;
         }
         case ACTION_ILLUMINATION: {
-          const illumination = data.readUInt16LE(7) / 100;
+          const illumination = data.readUInt32LE(7) / 100;
           const { onIllumination, site } = get(id);
           if (site) set(site, { illumination });
           set(id, { illumination });
