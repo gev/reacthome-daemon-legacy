7ca461ea2f195968db889d87bf7d122a4b2569b0
Evgeny <gazdovsky@gmail.com>
2019-10-09 18:38:12 +0300

    up

--- src/init/get.js
+++ src/init/get.js
@@ -5,7 +5,7 @@ const { ACTION_SET } = require('../constants');
 const sendAsset = require('../assets/send');
 
 
-module.exports = ({ state, assets }, session) => {
+module.exports = ({ state = [], assets = [] }, session) => {
   state.forEach(id => {
     sendAction({ type: ACTION_SET, id,  payload: get(id) });
   });
