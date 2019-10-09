6afb354b05076ce0da333bc13a76d221166cbb49
Evgeny <gazdovsky@gmail.com>
2019-10-09 15:01:54 +0300

    up

--- src/assets/send.js
+++ src/assets/send.js
@@ -5,23 +5,22 @@ const { sendAsset } = require('../webrtc/peer');
 
 const highWaterMark = 16384;
 
-let transaction = 0;
-
 module.exports = async (id, name) => {
   const file = asset(name);
   const s = await stat(file);
   if (s.isFile()) {
     let i = 1;
+    const transaction = BigInt(Date.now());
     const buff = Buffer.from(name);
     const m = s.size % highWaterMark;
     const total = m === 0 ? (s.size / highWaterMark) : (((s.size - m) / highWaterMark) + 1);
     const stream = fs.createReadStream(file, { highWaterMark });
     stream.on('readable', () => {
-      const header = Buffer.alloc(4 + 2 + 2 + 2);
+      const header = Buffer.alloc(8 + 2 + 2 + 2);
       header.writeBigUInt64LE(transaction, 0);
-      header.writeUInt16LE(total, 4);
-      header.writeUInt16LE(i, 6);
-      header.writeUInt16LE(buff.length, 8);
+      header.writeUInt16LE(total, 8);
+      header.writeUInt16LE(i, 10);
+      header.writeUInt16LE(buff.length, 12);
       const chunk = stream.read(highWaterMark);
       if (chunk) {
         sendAsset(id, Buffer.concat([header, buff, chunk]));
@@ -29,6 +28,5 @@ module.exports = async (id, name) => {
       }
     });
     stream.on('error', console.error);
-    transaction += 1;
   }
 };
