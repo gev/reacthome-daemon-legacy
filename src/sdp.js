0dc49d4f330641bcffcfbd1f70e8ae6e4b7b0094
Evgeny <gazdovsky@gmail.com>
2019-10-11 22:13:36 +0300

    up

--- src/sdp.js
+++ src/sdp.js
@@ -1,8 +1,6 @@
 
 const SDP = require('sdp-transform');
 
-module.exports.isUUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
 module.exports.fixSDP = (sdp) => {
   const o = SDP.parse(sdp);
   for (let media of o.media) {
