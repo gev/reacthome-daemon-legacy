34ac43a689e220ab8ab57efff87b67bd0656824b
Evgeny <gazdovsky@gmail.com>
2019-10-18 15:10:38 +0300

    up

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -387,9 +387,9 @@ const run = (action) => {
       case ACTION_DIM: {
         const { id, value } = action;
         const o = get(id) || {};
-        const { last, r, g, b, hsv: { h = 0, s = 0 } = {} } = o;
+        const { last, hsv: { h = 0, s = 0 } = {} } = o;
         const rgb = color.hsv.rgb(h, s, value / 2.55);
-        bind.forEach((i, c) => {
+        rgb.forEach((i, c) => {
           if (!o[i]) return;
           const { velocity } = get(o[i]) || {};
           const [dev,,index] = o[i].split('/');
