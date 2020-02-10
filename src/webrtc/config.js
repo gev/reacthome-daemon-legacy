9729660bd5df93bb1e4d59d35b6e4e5cd9c2b283
Evgeny <gazdovsky@gmail.com>
2020-02-10 20:21:19 +0300

    return test ice servers

--- src/webrtc/config.js
+++ src/webrtc/config.js
@@ -4,12 +4,16 @@ module.exports.gateURL = id => `wss://gate.reacthome.net/${id}`;
 module.exports.ICE = {
   iceServers: [
     {
-      urls: 'stun:gate.reacthome.net'
+      // urls: 'stun:gate.reacthome.net'
+      urls: 'stun:stun.l.google.com:19302'
     },
     {
-      urls: 'turns:gate.reacthome.net',
-      username: 'username',
-      credential: 'password'
+      // urls: 'turns:gate.reacthome.net',
+      // username: 'username',
+      // credential: 'password'
+      urls: 'turn:numb.viagenie.ca',
+      username: 'webrtc@live.com',
+      credential: 'muazkh'
     }
   ]
 };
