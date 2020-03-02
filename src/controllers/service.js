b9b45813942780d4d8ab398e095877b3723f1411
Evgeny <gazdovsky@gmail.com>
2020-03-02 20:00:21 +0300

    fix zero values

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -181,8 +181,8 @@ const run = (action) => {
         const { value, delay } = get(channel) || {};
         buffer.writeUInt8(ACTION_GROUP, 0);
         buffer.writeUInt8(action.index, 1);
-        buffer.writeUInt8(action.value || value, 2);
-        buffer.writeUInt16LE(action.delay || delay, 3);
+        buffer.writeUInt8(action.value === undefined ? value : action.value, 2);
+        buffer.writeUInt16LE(action.delay === undefined ? delay : action.delay, 3);
         device.send(buffer, dev.ip);
         break;
       }
