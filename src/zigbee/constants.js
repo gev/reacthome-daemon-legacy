f4a09e2bc3361e8e674b9142846d6c89bfe68784
Evgeny <gazdovsky@gmail.com>
2020-03-12 20:18:46 +0300

    typo

--- src/zigbee/constants.js
+++ src/zigbee/constants.js
@@ -2,5 +2,5 @@
 const { zigbee } = require('../fs');
 
 module.exports.ZIGBEE = 'zigbee';
-module.exports.serialPort = { path: './dev/ttyAMA0' };
+module.exports.serialPort = { path: '/dev/ttyAMA0' };
 module.exports.databasePath = zigbee('devices.json');
