a3b8666e8050fa55574f20c12a74e5427a799861
Evgeny <gazdovsky@gmail.com>
2020-03-13 03:06:56 +0300

    up

--- src/zigbee/in.js
+++ src/zigbee/in.js
@@ -1,6 +1,11 @@
 
-const { set } = require('../actions');
-const { DO } = require('../constants');
+const { set, count_on, count_off } = require('../actions');
+const { DO, onOn, onOff } = require('../constants');
+const { run } = require('../controllers/service');
+
+const onDI = [onOff, onOn, onHold, onClick];
+const onDO = [onOff, onOn];
+const count = [count_off, count_on];
 
 module.exports = (id, { ID, clusters }) => {
   Object
@@ -10,7 +15,17 @@ module.exports = (id, { ID, clusters }) => {
         case 'genOnOff': {
           const channel = `${id}/${DO}/${ID}`;
           set(channel, { value: attributes.onOff });
-          break;
+          const { bind, value } = get(channel);;
+          if (bind) {
+            if (value !== attributes.onOff) {
+              const script = chan[onDO[attributes.onOff]];
+              if (script) {
+                run({ type: ACTION_SCRIPT_RUN, id: script });
+              }
+              count[attributes.onOff](bind);
+            }
+          }
+        break;
         }
       }
     });
