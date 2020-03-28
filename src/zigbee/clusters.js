82dd1b990e8512047a603214c7522e7b4667ae83
Evgeny <gazdovsky@gmail.com>
2020-03-28 16:04:50 +0300

    up

--- src/zigbee/clusters.js
+++ src/zigbee/clusters.js
@@ -10,7 +10,7 @@ const type = (res) => async () => {
 
 const configure = (res, cluster, config) => async (endpoint) => {
   try {
-    // await endpoint.bind(cluster, controller.getDevicesByType('Coordinator')[0].getEndpoint(1));
+    await endpoint.bind(cluster, controller.getDevicesByType('Coordinator')[0].getEndpoint(1));
     await endpoint.configureReporting(cluster, config, {disableDefaultResponse: true});
   } catch (e) {
     console.error(e);
