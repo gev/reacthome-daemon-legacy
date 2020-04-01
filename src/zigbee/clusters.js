ff57b266bd051d795df4a89dcd4dc649b50eef6f
Evgeny <gazdovsky@gmail.com>
2020-04-01 11:56:01 +0300

    up

--- src/zigbee/clusters.js
+++ src/zigbee/clusters.js
@@ -11,8 +11,8 @@ const type = (res) => () => {
 const bind = async (endpoint, cluster, config) => {
   if (endpoint.binds.length > 0) return;
   try {
-    await endpoint.bind(cluster, controller.getDevicesByType('Coordinator')[0].getEndpoint(1));
-    await endpoint.configureReporting(cluster, config);
+    // await endpoint.bind(cluster, controller.getDevicesByType('Coordinator')[0].getEndpoint(1));
+    // await endpoint.configureReporting(cluster, config);
     console.log('bind', endpoint, cluster);
   } catch (e) {
     console.error(e);
