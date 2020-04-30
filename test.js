d5a199287443bdaccac5b814a71fb308d5e3aa23
Evgeny <gazdovsky@gmail.com>
2020-04-30 17:47:45 +0300

    up

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 
 
-let b = 0
-const a = [];
-for (let i = 0; i < 1000000; i++) {
-  const x =  Math.random();
-  a.push(x);
-  if (a.length > 512) a.shift();
-  b = (((a.length * 2 - 1) * b) + x) / (a.length * 2);
-  console.log(a.reduce((a, b) => a + b) / a.length, b);
-}
+const { RTCPeerConnection, RTCIceCandidate } = require('wrtc');
+const {ICE, options} = require('./src/webrtc/config');
+
+
+setTimeout(() => {
+  const peer = new RTCPeerConnection();
+  setTimeout(() => {
+    peer.close();
+  }, 5000);
+}, 10000);
