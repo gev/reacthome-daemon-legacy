03e5b0ded02a6bf42f82fe9558d39ad9d1986adb
Evgeny <gazdovsky@gmail.com>
2020-05-03 17:39:09 +0300

    up

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const { RTCPeerConnection } = require('wrtc');
 setInterval(() => {
 // setTimeout(() => {
   // for (let i = 0; i < 1; i++) {
-    const peer = new RTCPeerConnection();
+    let peer = new RTCPeerConnection();
     setTimeout(() => {
       peer.close();
       peer = null;
