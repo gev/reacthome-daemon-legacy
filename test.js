d394591efd5f95d3882c7d8e3e2c9c1ea8ae3afb
Evgeny <gazdovsky@gmail.com>
2020-05-03 16:21:35 +0300

    up

--- test.js
+++ test.js
@@ -4,8 +4,9 @@ setInterval(() => {
 // setTimeout(() => {
   // for (let i = 0; i < 1; i++) {
     const peer = new RTCPeerConnection();
-    // setTimeout(() => {
-    //   peer.close();
-    // }, 1000);
+    setTimeout(() => {
+      peer.close();
+      gc();
+    }, 5000);
   // }
 }, 10000);
