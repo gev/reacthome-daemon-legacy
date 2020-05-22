6fd6e7194a550558b387db0924551a64c9c69206
Evgeny <gazdovsky@gmail.com>
2020-05-22 19:43:28 +0300

    up

--- src/camera/index.js
+++ src/camera/index.js
@@ -25,16 +25,17 @@ module.exports.onWatch = ({ id, preview, audio = false, video = true }, session)
       const watch = (stream_id) => {
         console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
         console.log(stream_id)
-        janus.send(session_id, handle_id, { request: WATCH, id: stream_id }, ({ jsep }) => {
+        janus.send(session_id, handle_id, { request: WATCH, id: stream_id }, (data) => {
           console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-        if (jsep) {
-            jsep.sdp = fixSDP(jsep.sdp);
-            // jsep.sdp = jsep.sdp.replace('42801E', '42e01f');
-            // jsep.sdp = jsep.sdp.replace('420029', '42e01f');
-            console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-            console.log(jsep);
-            send(session, { type: WATCH, id, session_id, handle_id, stream_id, jsep });
-          }
+          console.log(data);
+          // if (jsep) {
+          //   jsep.sdp = fixSDP(jsep.sdp);
+          //   // jsep.sdp = jsep.sdp.replace('42801E', '42e01f');
+          //   // jsep.sdp = jsep.sdp.replace('420029', '42e01f');
+          //   console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+          //   console.log(jsep);
+          //   send(session, { type: WATCH, id, session_id, handle_id, stream_id, jsep });
+          // }
         });
       };
       if (streams.has(url)) {
