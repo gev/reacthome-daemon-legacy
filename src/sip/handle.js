b49e5bc4762adfce2e630165b92b44c4e18ad50b
Evgeny <gazdovsky@gmail.com>
2020-06-01 04:01:15 +0300

    up

--- src/sip/handle.js
+++ src/sip/handle.js
@@ -5,7 +5,8 @@ const SDP = require('sdp-transform');
 const uuid = require('uuid/v4');
 const janus = require('../janus');
 const { fixSDP } = require('../sdp');
-const { broadcast } = require('../notification');
+// const { broadcast } = require('../notification');
+const { broadcast } = require('../websocket/peer');
 const { PROCESS } = require('../janus/constants');
 const { INVITE, BYE, CANCEL, HANGUP } = require('./constants');
 const { get } = require('../actions')
@@ -100,7 +101,7 @@ module.exports.onInvite = (request) => {
           const o = SDP.parse(jsep.sdp);
           o.media = o.media.filter(media => media.type === 'audio');
           jsep.sdp = SDP.write(o);
-          console.log({ type: INVITE, jsep, session_id, handle_id, call_id, from });
+          broadcast({ type: INVITE, jsep, session_id, handle_id, call_id, from });
         }
       });
     });
