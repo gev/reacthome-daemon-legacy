d77cf7a9691248f80dd5a02f82eedc164122937b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-06-18 00:49:30 +0300

    up

--- src/camera/index.js
+++ src/camera/index.js
@@ -54,6 +54,7 @@ module.exports.onWatch = ({ id, preview, audio = false, video = true }, session)
 };
 
 module.exports.onStart = ({ session_id, handle_id, jsep }) => {
+  console.log(jsep);
   janus.send(session_id, handle_id, { request: START }, jsep, (data) => {
     console.log('data', JSON.stringify(data, null, 2));
   });
