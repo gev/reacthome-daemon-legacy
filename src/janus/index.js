264b9c58d58e393982e7bf5e54b22b9b99de7c64
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-06-26 14:59:19 +0300

    up

--- src/janus/index.js
+++ src/janus/index.js
@@ -1,10 +1,70 @@
 
-const { connect, send, bind } = require('./janus');
-const { CREATE, ATTACH, MESSAGE, TRICKLE } =require('./constants');
+const WebSocket = require('ws');
+const uuid = require('uuid/v4');
+const peer = require('../websocket/peer');
+const { streams } = require('../camera/streams');
+const { CREATE, ATTACH, MESSAGE, TRICKLE, CANDIDATE } =require('./constants');
 
-module.exports.start = connect;
+const TIMEOUT_RECONNECT = 1000;
+const TIMEOUT_TRANSACTION = 30000;
+const TIMEOUT_TRICKLE = 60000;
 
+const callbacks = new Map();
+const handlers = new Map();
+
+let socket;
 let session_id;
+
+const connect = () => {
+  session_id = null;
+  socket = new WebSocket('ws://localhost:8188', 'janus-protocol');
+  socket.on('message', (message) => {
+    try {
+      const action = JSON.parse(message);
+      console.log(JSON.stringify(action, null, 2));
+      if (action.transaction) {
+        if (callbacks.has(action.transaction)) {
+          const callback = callbacks.get(action.transaction);
+          callback(action);
+        }
+      } else if (action.janus === TRICKLE) {
+        const {session_id, sender: handle_id, candidate} = action;
+        const session = handlers.get(handle_id);
+        peer.send(session, {type: CANDIDATE, session_id, handle_id, candidate});
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  });
+  socket.on('close', () => {
+    setTimeout(connect, TIMEOUT_RECONNECT);
+  });
+  socket.on('error', console.error);
+  callbacks.clear();
+  streams.clear();
+};
+
+const send = (action, callback) => {
+  try {
+    const transaction = uuid();
+    if (callback) {
+      callbacks.set(transaction, callback);
+      setTimeout(() => {
+        // callbacks.delete(transaction)
+      }, TIMEOUT_TRANSACTION);
+    }
+    socket.send(JSON.stringify({ ...action, transaction }));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports.bind = (handle_id, session) => {
+  handlers.set(handle_id, session);
+  setTimeout(() => {
+    // handlers.delete(handle_id);
+  }, TIMEOUT_TRICKLE)
+};
 
 module.exports.createSession = (callback) => {
   if (session_id) {
@@ -37,4 +97,4 @@ module.exports.trickle = ({ session_id, handle_id, candidate }, callback) => {
   send({ janus: TRICKLE, session_id, handle_id, candidate }, callback);
 };
 
-module.exports.bind = bind;
+module.exports.start = connect;
