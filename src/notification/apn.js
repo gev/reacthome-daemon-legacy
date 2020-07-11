c35b06ca3f8f9b12278a7fcf801c441915b9e721
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-07-12 02:59:19 +0300

    up

--- src/notification/apn.js
+++ src/notification/apn.js
@@ -1,18 +1,15 @@
 const apn = require('apn');
-const apnAccount = require('../../var/apn.json');
+const account = require('../../var/apn.json');
 
-const tokens = new Map();
+const topic = 'net.reacthome';
 
 const provider = new apn.Provider({
-  token: apnAccount,
+  token: account,
   production: false
 });
 
-module.exports.send = (token, alert, topic, payload) => {
-  const note = new apn.Notification();
-  note.alert = alert;
-  note.topic = topic;
-  note.payload = payload;
+module.exports.send = (token, alert, body, payload) => {
+  const note = new apn.Notification({topic, alert, body, payload});
   provider.send(note, token)
     .then(res => console.log(JSON.stringify(res, null, 2)))
     .catch(console.error);
--- src/notification/firebase.js
+++ src/notification/firebase.js
@@ -1,15 +1,15 @@
 
 const firebase = require('firebase-admin');
-const firebaseAccount = require('../../var/firebase.json');
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(firebaseAccount),
-  databaseURL: 'https://reacthome-9021b.firebaseio.com'
-});
+const account = require('../../var/firebase.json');
 
 const params = {
   priority: 'high',
 };
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(account),
+  databaseURL: 'https://reacthome-9021b.firebaseio.com'
+});
 
 module.exports.send = (token, title, body, data) => {
   const message = {
