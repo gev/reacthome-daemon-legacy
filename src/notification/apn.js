dd23b9351429655b6168017b03ca9fd07f07cf17
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-07-12 16:46:47 +0300

    up

--- src/notification/apn.js
+++ src/notification/apn.js
@@ -1,6 +1,7 @@
 const apn = require('apn');
 const account = require('../../var/apn.json');
 
+const contentAvailable = 1;
 const topic = 'net.reacthome';
 const sound = 'default'
 
@@ -10,9 +11,13 @@ const provider = new apn.Provider({
 });
 
 module.exports.send = (token, title, body, payload) => {
-  const message = new apn.Notification({
-    topic, title, body, sound, payload, contentAvailable: true
-  });
+  const message = new apn.Notification();
+  message.contentAvailable = contentAvailable;
+  message.payload = payload;
+  message.topic = topic;
+  // const message = new apn.Notification({
+  //   topic, title, body, sound, payload, contentAvailable
+  // });
   provider.send(message, token)
     .catch(console.error);
 };
