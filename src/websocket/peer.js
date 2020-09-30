bd998461d6c3ed79446f68ce2cb2ced88f98b0e9
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-09-30 04:12:38 +0300

    up

--- src/websocket/peer.js
+++ src/websocket/peer.js
@@ -1,19 +1,6 @@
 
 const peers = new Map();
 
-const TIMEOUT  = 20000;
-const INTERVAL = 10000;
-
-setInterval(() => {
-  for (const peer of peers.values()) {
-    if (!peer.online) return;
-    const dt = Date.now() - peer.timestamp;
-    if (dt > TIMEOUT) {
-      peer.online = false;
-    }
-  }
-}, INTERVAL);
-
 module.exports.peers = peers;
 
 module.exports.send = (session, message) => {
