8d28892f4283fe9fca167533ed956a07c78cad73
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-10-10 20:02:12 +0300

    debug

---
+++ src/modbus/index.js
@@ -0,0 +1,26 @@
+
+const 
+
+module.exports.readCoils = (address, length) => {
+};
+
+module.exports.readDiscreteInputs = (address, length) => {
+};
+
+module.exports.readHoldingRegisters = (id, address, length) => {
+};
+
+module.exports.readInputRegisters = (id, address, length) => {
+};
+
+module.exports.writeCoil = (id, address, state) => {
+};
+
+module.exports.writeCoils = (id, address, array) => {
+};
+
+module.exports.writeRegister = (id, address, value) => {
+};
+
+module.exports.writeRegisters = (id, address, array) => {
+};
