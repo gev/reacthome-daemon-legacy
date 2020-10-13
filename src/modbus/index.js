74e9457ee2a3ceb2d90102e33beddd2503958115
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-10-13 13:05:56 +0300

    up

--- src/modbus/index.js
+++ src/modbus/index.js
@@ -1,6 +1,7 @@
 
 const {get} = require('../actions');
 const { ACTION_RS485_TRANSMIT } = require('../constants');
+const crc16 = require('../crc16');
 const { send } = require('../sockets/device');
 
 module.exports.readCoils = (id, address, length) => {
@@ -34,22 +35,6 @@ module.exports.writeRegisters = (id, address, array) => {
 
 let i = 0;
 
-const crc = buffer => {
-  var crc = 0xFFFF;
-  var odd;
-  for (var i = 2; i < buffer.length; i++) {
-      crc = crc ^ buffer[i];
-      for (var j = 0; j < 8; j++) {
-          odd = crc & 0x0001;
-          crc = crc >> 1;
-          if (odd) {
-              crc = crc ^ 0xA001;
-          }
-      }
-  }
-  return crc;
-}
-
 module.exports.start = () => {
   setInterval(() => {
     const buffer = Buffer.alloc(10);
@@ -59,7 +44,7 @@ module.exports.start = () => {
     buffer.writeUInt8(0x6, 3);
     buffer.writeUInt16BE(1, 4);
     buffer.writeUInt16BE(3, 6);
-    buffer.writeUInt16BE(crc(buffer), 8);
+    buffer.writeUInt16BE(crc16(buffer), 8);
     console.log(buffer);
     send(buffer, '172.16.0.14');
   }, 1000);
