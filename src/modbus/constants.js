78f8cb78da2c6843d9eed243f4426268c0bdf74c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-10-19 14:19:15 +0300

    fix

--- src/modbus/constants.js
+++ src/modbus/constants.js
@@ -1,5 +1,5 @@
 
-module.exports.READ_HOLDING_REGISTERS = 0x3;
-module.exports.READ_INPUT_REGISTERS = 0x4;
-module.exports.WRITE_REGISTER = 0x6;
-module.exports.WRITE_REGISTERS = 0xa;
+module.exports.READ_HOLDING_REGISTERS = 0x03;
+module.exports.READ_INPUT_REGISTERS = 0x04;
+module.exports.WRITE_REGISTER = 0x06;
+module.exports.WRITE_REGISTERS = 0x10;
