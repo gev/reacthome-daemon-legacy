110b17fcbe62f8bb7bb36990630085b641bd5a39
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-10-26 01:39:29 +0300

    up

--- src/drivers/varmann/index.js
+++ src/drivers/varmann/index.js
@@ -24,8 +24,6 @@ const sync = (id) => {
   }
 };
 
-const setAddress = write(0x0, BROADCAST_ADDRESS);
-
 module.exports.handle = (action) => {
   switch (action.type) {
     case ACTION_SET_ADDRESS: {
