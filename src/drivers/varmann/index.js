c2c0baf946d4672264b88590ae446fa0c6d1154c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-10-26 01:41:26 +0300

    up

--- src/drivers/varmann/index.js
+++ src/drivers/varmann/index.js
@@ -2,7 +2,7 @@
 const { get, set } = require('../../actions');
 const { ACTION_SET_ADDRESS, ACTION_SET_FAN_SPEED } = require('../../constants');
 const { writeRegister, readHoldingRegisters } = require('../modbus');
-const { READ_HOLDING_REGISTERS } = require('../modbus/constants');
+const { READ_HOLDING_REGISTERS, WRITE_REGISTER } = require('../modbus/constants');
 const { BROADCAST_ADDRESS, TIMEOUT } = require('./constants');
 
 const instance = new Set();
@@ -37,6 +37,9 @@ module.exports.handle = (action) => {
     default: {
       const {id, data} = action;
       switch (data[0]) {
+        case WRITE_REGISTER: {
+          set(id, {synced: true});
+        }
         case READ_HOLDING_REGISTERS: {
           set(id, {fan_speed: data.readUInt16BE(14), synced: true})
           break;
