7a8e1bccc06645cd493c932857296793f16ff03f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-11-25 23:04:06 +0300

    spawn ffmpeg

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
-const { RTCPeerConnection } = require('wrtc');
 
-setInterval(() => {
-// setTimeout(() => {
-  // for (let i = 0; i < 1; i++) {
-    let peer = new RTCPeerConnection();
-    setTimeout(() => {
-      peer.close();
-      peer = null;
-      // gc();
-    }, 3000);
-  // }
-}, 5000);
+const {spawn} = require('child_process');
+
+spawn('ffpmpeg', [
+  '-re', '-f', 'lavfi', '-i', '"sine=frequency=2133:sample_rate=48000:duration=25"', 
+  '-f', 's16le', '-ar', '48000', '-ac', '2', 
+]).stdout.on('data', (data) => {
+  console.log(data.length);
+})
