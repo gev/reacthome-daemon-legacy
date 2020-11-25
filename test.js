d55f2a1fe399c372c7a149bd2da1c8593f93229a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-11-25 23:06:25 +0300

    up

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 
 const {spawn} = require('child_process');
 
-spawn('ffpmpeg', [
+const ffmpeg = spawn('ffpmpeg', [
   '-re', '-f', 'lavfi', '-i', '"sine=frequency=2133:sample_rate=48000:duration=25"', 
-  '-f', 's16le', '-ar', '48000', '-ac', '2', 
-]).stdout.on('data', (data) => {
+  '-f', 's16le', '-ar', '48000', '-ac', '2',
+  '-', 
+]);
+ffmpeg.on('error', console.error);
+ffmpeg.stdout.on('data', (data) => {
   console.log(data.length);
 })
