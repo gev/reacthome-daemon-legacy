04fb121613aa6b84fdb708c007e87f3689230a6d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-11-25 23:10:10 +0300

    typotypo

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 
 const {spawn} = require('child_process');
 
-const ffmpeg = spawn('ffpmpeg', [
+const ffmpeg = spawn('ffmpeg', [
   '-re', '-f', 'lavfi', '-i', '"sine=frequency=2133:sample_rate=48000:duration=25"', 
   '-f', 's16le', '-ar', '48000', '-ac', '2',
   '-', 
