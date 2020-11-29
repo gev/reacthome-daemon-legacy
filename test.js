a8e88d3db8c9ad806b51d5ede8fb45935ea458e4
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-11-30 02:42:33 +0300

    typo

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const shair = spawn('shairport-sync', ['-o', 'stdout']);
 shair.on('error', console.error);
 
 const ffmpeg = spawn('ffmpeg', [
-  '-re', '-f', 's16le', '-ar', '4410', '-ac', '2', '-i', '-', 
+  '-re', '-f', 's16le', '-ar', '44100', '-ac', '2', '-i', '-', 
   '-f', 'rtp', '-c:a', 'pcm_s16le', '-ar', '48000', '-ac', '2',
   'rtp://127.0.0.1:1234', 
 ]);
