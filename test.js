b896eec4fb36e3a19623947148ac558b72f8b9d7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-11-30 02:35:43 +0300

    up

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 
 const {spawn} = require('child_process');
 
+const shair = spawn('shairport-sync', ['-o', 'stdout']);
+shair.on('error', console.error);
+
 const ffmpeg = spawn('ffmpeg', [
-  '-re', '-f', 'lavfi', '-i', '"sine=frequency=2133:sample_rate=48000:duration=25"', 
-  '-f', 's16le', '-ar', '48000', '-ac', '2',
-  '-', 
+  '-re', '-f', 's16le', '-ar', '4410', '-ac', '2', '-i', '-', 
+  '-f', 'rtp', '-c:a', 'pcm_s16le', '-ar', '48000', '-ac', '2',
+  'rtp://127.0.0.1:1234', 
 ]);
 ffmpeg.on('error', console.error);
-ffmpeg.stdout.on('data', (data) => {
-  console.log(data);
-});
-setInterval(() => {}, 1000);
+
+shair.stdout.pipe(ffmpeg.stdin)
