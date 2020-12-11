e92536e5250fccb64e3d0398977c747a82736ed4
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-12-11 03:24:23 +0300

    up

--- src/drivers/owen/me210_701.js
+++ src/drivers/owen/me210_701.js
@@ -12,7 +12,7 @@ for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 6; j++) {
     setTimeout(() => {
       const a = 5240 + i * 2 + j * 12;
-      consule.log(a);
+      console.log(a);
       readHoldingRegisters('172.16.1.1', 502, 1, a, 2);
     }, k);
     k += 2000;
