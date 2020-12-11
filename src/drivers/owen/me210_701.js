96fc31ed16c662321bc069515fd3b77b8e2a2001
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-12-11 03:08:57 +0300

    typo

--- src/drivers/owen/me210_701.js
+++ src/drivers/owen/me210_701.js
@@ -8,7 +8,7 @@ module.exports.add = (id) => {
 
 readHoldingRegisters('172.16.1.1', 502, 1, 5336, 2);
 for (let i = 0; i < 3; i++) {
-  for (let i = 0; j < 6; i++) {
+  for (let j = 0; j < 6; j++) {
     readHoldingRegisters('172.16.1.1', 502, 1, 5240 + i * 2 + j * 12, 2);
   }
 }
