a92c20d68bc11af7103992a94bfcc433fb75327e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-12-12 23:31:25 +0300

    up

--- src/discovery.js
+++ src/discovery.js
@@ -10,17 +10,19 @@ const CLIENT_PORT = 2021;
 const CLIENT_GROUP = '224.0.0.2';
 const TIMEOUT = 1000;
 
+const {addres} = networkInterfaces().eth1[0];
+const socket = createSocket('udp4');
+socket.on('error', console.error);
+socket.bind({address}, () => {
+  socket.setMulticastInterface(address);
+});
+
 module.exports.start = (id) => {
-  const socket = createSocket('udp4');
-  socket.on('error', console.error);
-  socket.bind(() => {
-    socket.setMulticastInterface(networkInterfaces().eth1[0].address)
-    setInterval(() => {
-      socket.send(JSON.stringify({
-        id,
-        type: DISCOVERY,
-        payload: get(id)
-      }), CLIENT_PORT, CLIENT_GROUP);
-    }, TIMEOUT);
-  });
+  setInterval(() => {
+    socket.send(JSON.stringify({
+      id,
+      type: DISCOVERY,
+      payload: get(id)
+    }), CLIENT_PORT, CLIENT_GROUP);
+  }, TIMEOUT);
 };
