bed713ab4e3ed119bbea8ca8a3004124ca8a0e9b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2020-12-13 17:04:53 +0300

    up

--- src/discovery.js
+++ src/discovery.js
@@ -1,4 +1,3 @@
-
 const { networkInterfaces } = require('os');
 const { createSocket } = require('dgram');
 const { get } = require('./actions');
@@ -6,12 +5,12 @@ const { get } = require('./actions');
 const DISCOVERY = 'discovery';
 const CLIENT_GROUP = '224.0.0.2';
 const CLIENT_PORT = 2021;
-const ADDRESS = networkInterfaces().eth1[0].address;
 
 module.exports.start = (id) => {
   const socket = createSocket('udp4');
   socket.on('error', console.error);
   socket.on('message', (message, {port, address}) => {
+    console.log(message, port, address);
     try {
       const { type } = JSON.parse(Buffer.from(message));
       if (type === DISCOVERY) {
@@ -28,5 +27,5 @@ module.exports.start = (id) => {
   socket.on('listening', () => {
     socket.addMembership(CLIENT_GROUP);
   });
-  socket.bind({address: ADDRESS, port: CLIENT_PORT});
+  socket.bind(CLIENT_PORT);
 };
