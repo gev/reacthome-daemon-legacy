b7e9ccbfbc2c73932485ff1467dc35189238e673
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-03-26 20:46:18 +0300

    up

--- src/sockets/device.js
+++ src/sockets/device.js
@@ -17,6 +17,6 @@ const device = socket((socket) => {
   return () => {
     device.send(data, DEVICE_GROUP);
   };
-}, DISCOVERY_INTERVAL, DEVICE_PORT, DEVICE_SERVER_PORT, '172.16.0.1', false);
+}, DISCOVERY_INTERVAL, DEVICE_PORT, DEVICE_SERVER_PORT, '172.16.0.1', true);
 
 module.exports = device;
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,7 +1,7 @@
 
 const { createSocket } = require('dgram');
 
-module.exports = (discovery, interval, port, listen, multicast, hasQueue, delay = 20) => {
+module.exports = (discovery, interval, port, listen, multicast, hasQueue, delay = 100) => {
 
   const timer = {};
   const queue = {};
@@ -9,41 +9,41 @@ module.exports = (discovery, interval, port, listen, multicast, hasQueue, delay 
 
   const socket = createSocket('udp4');
 
-  const send = (packet, ip) => {
+  const _send = (packet, ip) => {
     socket.send(packet, port, ip, (err) => {
       if (err) console.error(err);
     });
   };
 
-  // const send = hasQueue
-  //   ? (packet, ip) => {
-  //     if (!timer[ip]) {
-  //       queue[ip] = [];
-  //       timestamp[ip] = Date.now();
-  //       timer[ip] = setInterval(() => {
-  //         const q = queue[ip];
-  //         if (q.length === 0) return;
-  //         const now = Date.now();
-  //         if (now - timestamp[ip] < delay) return;
-  //         timestamp[ip] = now;
-  //         _send(q.shift(), ip);
-  //       }, 20);
-  //     } else {
-  //       const q = queue[ip];
-  //       if (q.length === 0) {
-  //         const now = Date.now();
-  //         if (now - timestamp[ip] > delay) {
-  //           timestamp[ip] = now;
-  //           _send(packet, ip);
-  //         } else {
-  //           q.push(packet);
-  //         }
-  //       } else {
-  //         q.push(packet);
-  //       }
-  //     }
-  //   }
-  //   : _send;
+  const send = hasQueue
+    ? (packet, ip) => {
+      if (!timer[ip]) {
+        queue[ip] = [];
+        timestamp[ip] = Date.now();
+        timer[ip] = setInterval(() => {
+          const q = queue[ip];
+          if (q.length === 0) return;
+          const now = Date.now();
+          if (now - timestamp[ip] < delay) return;
+          timestamp[ip] = now;
+          _send(q.shift(), ip);
+        }, 20);
+      } else {
+        const q = queue[ip];
+        if (q.length === 0) {
+          const now = Date.now();
+          if (now - timestamp[ip] > delay) {
+            timestamp[ip] = now;
+            _send(packet, ip);
+          } else {
+            q.push(packet);
+          }
+        } else {
+          q.push(packet);
+        }
+      }
+    }
+    : _send;
 
   // const sendConfirm = (packet, ip, confirm, t = 1000) => {
   //   if (confirm()) return;
