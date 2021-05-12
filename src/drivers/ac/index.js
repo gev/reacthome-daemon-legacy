323356947d390ecf043ca1cda01f57e1ce8a4a2f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-05-12 13:22:17 +0300

    up

--- src/drivers/ac/index.js
+++ src/drivers/ac/index.js
@@ -1,7 +1,14 @@
-
-const { get, set } = require('../../actions');
-const { device } = require('../../sockets');
-const { ACTION_IR, ON, OFF, ACTION_ON, ACTION_OFF, ACTION_ENABLE, ACTION_DISABLE } = require('../../constants');
+const { get, set } = require("../../actions");
+const { device } = require("../../sockets");
+const {
+  ACTION_IR,
+  ON,
+  OFF,
+  ACTION_ON,
+  ACTION_OFF,
+  ACTION_ENABLE,
+  ACTION_DISABLE,
+} = require("../../constants");
 
 const code = (a, b, data) => {
   const code = [a, b];
@@ -15,13 +22,13 @@ const code = (a, b, data) => {
     }
   });
   return code;
-}
+};
 
 const frequency = 38000;
 
 const manage = (power, mode = 0, fan = 0, setpoint = 24, bind) => {
   if (!bind) return;
-  const [dev,,index] = bind.split('/');
+  const [dev, , index] = bind.split("/");
   const { ip } = get(dev) || {};
   if (!ip) return;
   const buff = Buffer.alloc(597);
@@ -30,9 +37,9 @@ const manage = (power, mode = 0, fan = 0, setpoint = 24, bind) => {
   buff.writeUInt8(0, 2);
   buff.writeUInt16BE(frequency, 3);
   const data = [0xf2, 0x0d, 0x03, 0xfc, 0x01];
-  let t = setpoint < 17 ? 0 : (setpoint - 17);
+  let t = setpoint < 17 ? 0 : setpoint - 17;
   data[5] = (t & 0xf) << 4;
-  data[6] = ((fan ? ((fan + 1) & 0x7) : 0 ) << 5) | (power ? (mode & 0x3) : 0x7);
+  data[6] = ((fan ? (fan + 1) & 0x7 : 0) << 5) | (power ? mode & 0x3 : 0x7);
   data[7] = 0;
   data[8] = data.reduce((a, b) => a ^ b);
   const ir = code(167, 164, data);
@@ -48,14 +55,14 @@ module.exports.handle = ({ type, id }) => {
   const ac = get(id) || {};
   let enabled = ac.enabled;
   const { setpoint } = get(ac.thermostat) || {};
-  const { value } = get(ac.bind) || {}
+  const { value } = get(ac.bind) || {};
   switch (type) {
     case ACTION_ENABLE:
       enabled = true;
       set(ac.bind, { value: ON });
     case ACTION_ON: {
       if (!enabled) return;
-      if (value === ON && ac.setpoint === setpoint) return;
+      if (value && ac.setpoint == setpoint) return;
       manage(ON, ac.mode, ac.fan, setpoint, ac.bind);
       set(id, { setpoint, enabled });
       break;
@@ -64,7 +71,7 @@ module.exports.handle = ({ type, id }) => {
       enabled = false;
       set(ac.bind, { value: OFF });
     case ACTION_OFF: {
-      if (value === OFF && ac.setpoint === setpoint) return;
+      // if (value == OFF) return;
       manage(OFF, ac.mode, ac.fan, setpoint, ac.bind);
       set(id, { setpoint, enabled });
       break;
