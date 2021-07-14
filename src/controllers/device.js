6fd291e87b7b3c8a257387c78f04eb3d95a3813b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-07-14 13:37:21 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -209,6 +209,7 @@ module.exports.manage = () => {
           const index = data[7];
           const channel = `${id}/${RS485}/${index}`;
           const { bind } = get(channel) || {};
+          console.log(data.slice(8));
           drivers.handle({ id: bind, data: data.slice(8) });
           break;
         }
