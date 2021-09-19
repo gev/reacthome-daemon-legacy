a46433ef4324f313fa0eabaab940682159311536
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-09-19 17:50:26 +0300

    zigbee panId

--- src/zigbee/controller.js
+++ src/zigbee/controller.js
@@ -1,15 +1,20 @@
-const { Controller } = require('zigbee-herdsman');
-const { zigbee } = require('../fs');
+const { Controller } = require("zigbee-herdsman");
+const { zigbee } = require("../fs");
+const { parse } = require("uuid");
 
-module.exports = new Controller({
-  databasePath: zigbee('devices.json'), 
-  serialPort: {
-    path: '/dev/ttyAMA0',
-    adapter: 'zstack',
-  }, 
-  network: {
-    channelList: [14],
-  },
-  concurrent: 16,
-});
-
+module.exports = (id) => {
+  const data = Array.from(parse(id));
+  return new Controller({
+    databasePath: zigbee("devices.json"),
+    serialPort: {
+      path: "/dev/ttyAMA0",
+      adapter: "zstack",
+    },
+    network: {
+      panID: data.slice(0, 2),
+      extendedPanID: data.slice(2, 10),
+      channelList: [14],
+    },
+    concurrent: 16,
+  });
+};
