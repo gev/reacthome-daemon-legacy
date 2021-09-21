b6ef9caf9cc69759506c093a9b1426d926bfacf8
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-09-21 06:42:29 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -359,10 +359,10 @@ module.exports.manage = () => {
           for (let i = 0; i < 2; i++) {
             volume[i] = data[i + 9];
             source[i] = [];
-            for (let j = 0; j < 9; j++) {
+            for (let j = 0; j < 5; j++) {
               source[i][j] = {
-                active: Boolean(data[i * 9 + j + 11]),
-                volume: data[i * 9 + j + 11 + 9 * 2],
+                active: Boolean(data[i * 5 + j + 11]),
+                volume: data[i * 5 + j + 11 + 5 * 2],
               };
             }
           }
