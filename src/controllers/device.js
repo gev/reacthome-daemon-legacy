6d5efc60c0440b2f1b8be7796739d3e0543e870c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-10-06 18:51:07 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -280,7 +280,7 @@ module.exports.manage = () => {
         }
         case ACTION_TEMPERATURE: {
           const temperature = data.readUInt16LE(7) / 100;
-          const { onTemperature, site } = get(id);
+          const { onTemperature, site } = get(id) || {};
           if (site) set(site, { temperature });
           set(id, { temperature });
           if (onTemperature) {
