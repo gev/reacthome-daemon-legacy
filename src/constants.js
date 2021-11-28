29d26c66405996a484593039dd1bfe4718dbedb7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-11-28 14:10:22 +0300

    up 6.6.4

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-module.exports.VERSION = "6.6.3";
+module.exports.VERSION = "6.6.4";
 
 module.exports.DEVICE_PORT = 2017;
 module.exports.DEVICE_SERVER_PORT = 2016;
