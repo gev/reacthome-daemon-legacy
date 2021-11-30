dd75cda54dd7482ca309505549bcd7f84abda6b0
Evgeny Gazdovsky <gazdovsky@gmail.com>
2021-11-30 14:18:24 +0300

    refactor herdsman startup

--- src/zigbee/index.js
+++ src/zigbee/index.js
@@ -27,18 +27,16 @@ const addDevice = (id, device) => {
   });
 };
 
-module.exports.start = (id) => {
+module.exports.start = async (id) => {
   const controller = createController(id);
   controller.on("deviceJoined", ({ device }) => {
     online(device.ieeeAddr, device.networkAddress);
     addDevice(id, device);
   });
-
   controller.on("deviceLeave", ({ device }) => {
     offline(device.ieeeAddr);
     del(id, DEVICE, device.ieeeAddr);
   });
-
   controller.on("deviceInterview", ({ device }) => {
     online(device.ieeeAddr, device.networkAddress);
     addDevice(id, device);
@@ -46,47 +44,19 @@ module.exports.start = (id) => {
       handle(device.ieeeAddr, endpoint);
     });
   });
-
   controller.on("deviceAnnounce", ({ device }) => {
-    // console.log('anonce', device);
     online(device.ieeeAddr, device.networkAddress);
   });
-
   controller.on("message", ({ device, endpoint, data }) => {
     online(device.ieeeAddr, device.networkAddress);
     handle(device.ieeeAddr, endpoint, data);
   });
-
-  controller.start().then(() => {
-    controller.permitJoin(true);
-    controller.setTransmitPower(22);
-    // controller.getNetworkParameters().then(param => {
-    //   console.log(JSON.stringify(param, null, 2));
-    // });
-    // console.log(controller.getDevices().length);
-    controller.getDevices().forEach((device) => {
-      // console.log('-----------------------------------------------------------------');
-      // console.log(JSON.stringify(device, null, 2));
-      // addDevice(id, device);
-      // device.endpoints.forEach(endpoint => {
-      //   handle(device.ieeeAddr, endpoint);
-      // });
-    });
-    //  setInterval(() => {
-    //    console.log('-----------------------------------------------------------------');
-    //    controller.getDevices().forEach(async device => {
-    //      const {code} = get(device.ieeeAddr) || {};
-    //      console.log(code || device.ieeeAddr);
-    //      try {
-    //        const lqi = await device.lqi();
-    //        console.log('lqi:', device.networkAddress, JSON.stringify(lqi, null, 2));
-    //        const table = await device.routingTable();
-    //        console.log('routing table:', device.networkAddress, JSON.stringify(table, null, 2));
-    //      } catch (e) {
-    //        // console.error(e);
-    //      }
-    //      console.log();
-    //    });
-    //  }, 60000);
-  });
+  try {
+    await controller.start();
+    // await controller.reset("soft");
+    await controller.permitJoin(true);
+    await controller.setTransmitPower(22);
+  } catch (e) {
+    console.error(e);
+  }
 };
