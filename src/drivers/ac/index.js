ebf756bdd779b9dcc375e4bdcc2129cd303a1cc0
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-06-03 15:26:18 +0300

    up

--- src/drivers/ac/index.js
+++ src/drivers/ac/index.js
@@ -17,9 +17,9 @@ const manage = (power, setpoint, ac) => {
   if (!ac.bind) return;
   const [dev, , index] = ac.bind.split("/");
   const { ip, type, version = "" } = get(dev) || {};
-  const model = (ircodes.codes.AC[ac.brand] || {})[ac.model] || {};
+  const model = (ircodes.codes.AC[ac.brand] || {})[ac.model];
+  if (!model) return;
   const command = model.command(power, setpoint);
-  if (!command) return;
   switch (type) {
     case DEVICE_TYPE_IR_4: {
       const [major] = version.split(".");
