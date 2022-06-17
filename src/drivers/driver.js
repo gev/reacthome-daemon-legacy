bdfadc62a101df8294c5a3024e8cbe3bbe67beb6
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-06-17 13:02:37 +0300

    up

--- src/drivers/driver.js
+++ src/drivers/driver.js
@@ -83,7 +83,8 @@ module.exports.manage = () => {
 };
 
 module.exports.handle = (action) => {
-  console.log(action);
+  console.log(run[action.id]);
+  //console.log(run[action.id].handle);
   if (run[action.id] && run[action.id].handle) {
     run[action.id].handle(action);
   }
