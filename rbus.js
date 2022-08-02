9a3cf75d05df0b0553bd9c698fc533a7f2d746b3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-02 23:20:23 +0300

    ??

--- rbus.js
+++ rbus.js
@@ -1,7 +1,7 @@
 const { rbus } = require("./src/rbus");
 
-rbus(1, '127.0.1.1', '/dev/ttyAMA2', 12);
-rbus(2, '127.0.1.2', '/dev/ttyAMA1', 16);
+rbus(1, '127.0.1.1', '/dev/ttyAMA2', 16);
+rbus(2, '127.0.1.2', '/dev/ttyAMA1', 12);
 
 // uart1 
 // rx 9
