09b3e15c8d77ef97b848b6574781f7ccea7e1b8b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-03 01:14:45 +0300

    typo

--- src/rbus/serial/handle/index.js
+++ src/rbus/serial/handle/index.js
@@ -2,7 +2,7 @@ const { handleRBUS } = require("./handleRBUS");
 const { handleRS485 } = require("./handleRS485");
 
 module.exports.handle = (rbus) => (data) => {
-  console.log(rbus.index, rbus.isRBUS, data);
+  console.log(rbus.index, rbus.port.isRBUS, data);
   if (rbus.port.isRBUS) {
     handleRBUS(rbus, data);
   } else {
