c8b54bccfdec405a6e1e42c255d12c5802302e3b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-02 20:50:00 +0300

    typo

--- src/rbus/pool.js
+++ src/rbus/pool.js
@@ -1,6 +1,6 @@
 module.exports.getAddress = (rbus, mac, type) => {
   let address = rbus.pool.findIndex(i => i.mac === mac);
-  if (address === undefined) {
+  if (address === -1) {
     address = rbus.pool.length + 1;
   }
   rbus.pool[address] = { mac, type };
