4daa722408162dcb276bb5efaf24aa750b42f985
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-03 01:00:14 +0300

    rbusTransmitConfirm

--- src/rbus/serial/handle/rbusTransmitConfirm.js
+++ src/rbus/serial/handle/rbusTransmitConfirm.js
@@ -1,7 +1,9 @@
+const { RBUS_CONFIRM_PREAMBLE_TRANSMIT } = require("../../constants");
 const { addCRC } = require("../../crc")
 
 module.exports.rbusTransmitConfirm = (rbus, address) => {
   rbus.port.sendRBUS(Array.from([
-
+    RBUS_CONFIRM_PREAMBLE_TRANSMIT,
+    address
   ]));
 }
