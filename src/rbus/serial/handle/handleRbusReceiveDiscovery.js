18b7a4602a006c9a0cba19959000ef71a98ee039
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-02 21:24:25 +0300

    transmit discovery

--- src/rbus/serial/handle/handleRbusReceiveDiscovery.js
+++ src/rbus/serial/handle/handleRbusReceiveDiscovery.js
@@ -2,6 +2,7 @@ const { RBUS_DISCOVERY_RX_BUFFER_SIZE } = require("../../constants");
 const { checkCRC } = require("../../crc");
 const { macS } = require("../../mac");
 const { getAddress } = require("../../pool");
+const { rbusTransmitDiscovery } = require("./rbusTransmitDiscovery");
 
 module.exports.handleRbusReceiveDiscovery = (rbus, data) => {
   console.log(rbus.index, data, checkCRC(data));
@@ -14,13 +15,11 @@ module.exports.handleRbusReceiveDiscovery = (rbus, data) => {
     const type = [data[7], data[8], data[9]];
     const address = getAddress(rbus, mac, type);
     console.log(mac, address, type);
-    // rbus_device_type_t * type = (rbus_device_type_t *)(buffer + RBUS_DISCOVERY_HEADER_SIZE + sizeof(mac_t));
-    //     int16_t address = rbus_mac_table_add_mac(& (rbus -> mac_table), mac, type);
-    // if (address >= 0)
-    //   rbus_transmit_discovery(rbus, address, mac);
+    if (address >= 0) {
+      rbusTransmitDiscovery(rbus, address, mac_);
+    } else {
+      //TODO error: max number devices on the line
+    }
+    return;
   }
-  else {
-    //TODO error: max number devices on the line
-  }
-  return;
 }
