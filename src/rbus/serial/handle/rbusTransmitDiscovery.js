ed1b9d9b9a9c7dbdc2c5a48579462b112f114a45
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-02 23:00:47 +0300

    typpo

--- src/rbus/serial/handle/rbusTransmitDiscovery.js
+++ src/rbus/serial/handle/rbusTransmitDiscovery.js
@@ -3,7 +3,7 @@ const { addCRC } = require("../../crc")
 
 module.exports.rbusTransmitDiscovery = (rbus, address, mac) => {
   // rbus.port.send
-  connsole.log(addCRC(Array.from([
+  console.log(addCRC(Array.from([
     RBUS_DISCOVERY_PREAMBLE_TRANSMIT,
     ...mac,
     address
