db3e01f7acc089b20816a71fc0e8118a419eff4c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-02 23:23:35 +0300

    ckeanup

--- src/rbus/serial/handle/rbusTransmit.js
+++ src/rbus/serial/handle/rbusTransmit.js
@@ -2,7 +2,6 @@ const { RBUS_ADDERSS } = require("../../constants")
 const { rbusTransmitPing } = require("./rbusTransmitPing")
 
 module.exports.rbusTransmit = (rbus) => {
-  console.log(rbus.index, rbus.pool, rbus.pool.length)
   if (rbus.pool.length === 0) {
     rbusTransmitPing(rbus, RBUS_ADDERSS);
   }
