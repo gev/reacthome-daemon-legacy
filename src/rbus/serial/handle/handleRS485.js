8781649c21a3f6556dc33cb77b37ebde5f2b1035
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-03 01:11:01 +0300

    up

---
+++ src/rbus/serial/handle/handleRS485.js
@@ -0,0 +1,3 @@
+module.exports.handleRS485 = (rbus, data) => {
+
+}
--- src/rbus/serial/handle/index.js
+++ src/rbus/serial/handle/index.js
@@ -1,29 +1,11 @@
-const {
-  RBUS_DATA_PREAMBLE_RECEIVE,
-  RBUS_DISCOVERY_PREAMBLE_RECEIVE,
-  RBUS_CONFIRM_PREAMBLE_RECEIVE,
-  RBUS_PING_PREAMBLE_RECEIVE
-} = require("../../constants");
-const { handleRbusReceiveConfirm } = require("./handleRbusReceiveConfirm");
-const { handleRbusReceiveData } = require("./handleRbusReceiveData");
-const { handleRbusReceiveDiscovery } = require("./handleRbusReceiveDiscovery");
-const { handleRbusReceivePing } = require("./handleRbusReceivePing");
+const { handleRBUS } = require("./handleRBUS");
+const { handleRS485 } = require("./handleRS485");
 
 module.exports.handle = (rbus) => (data) => {
   console.log(rbus.index, data);
-  const preambula = data[0];
-  switch (preambula) {
-    case RBUS_DATA_PREAMBLE_RECEIVE:
-      handleRbusReceiveData(rbus, data);
-      break;
-    case RBUS_DISCOVERY_PREAMBLE_RECEIVE:
-      handleRbusReceiveDiscovery(rbus, data);
-      break;
-    case RBUS_CONFIRM_PREAMBLE_RECEIVE:
-      handleRbusReceiveConfirm(rbus, data);
-      break;
-    case RBUS_PING_PREAMBLE_RECEIVE:
-      handleRbusReceivePing(rbus, data);
-      break;
+  if (rbus.isRBUS) {
+    handleRBUS(rbus, data);
+  } else {
+    handleRS485(rbus, data)
   }
 }
