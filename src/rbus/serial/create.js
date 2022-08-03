9af8ef36c37bb4e15f5ed71a611afab6d0f19dc7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-03 16:27:39 +0300

    up

--- src/rbus/serial/create.js
+++ src/rbus/serial/create.js
@@ -16,8 +16,9 @@ const createPort = (rbus, path, isRBUS, baudRate, lineControl) => {
   const send = (data) => {
     rbus.rede.write(1);
     port.write(data);
-    port.drain();
-    rbus.rede.write(0);
+    port.drain(() => {
+      rbus.rede.write(0);
+    });
   }
   rbus.port = {
     path, baudRate, lineControl, isRBUS,
