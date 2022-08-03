1e27c140eb1644e89052c6357bd663bbef68279e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-03 13:31:54 +0300

    up

--- src/rbus/serial/handle/rbusTaskTransmit.js
+++ src/rbus/serial/handle/rbusTaskTransmit.js
@@ -15,7 +15,7 @@ module.exports.rbusTaskTransmit = (rbus) => {
       RBUS_DATA_PREAMBLE_TRANSMIT,
       item.address,
       item.id,
-      item.data.size,
+      item.data.length,
       ...item.data
     ])
   } else {
