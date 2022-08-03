7991bba9064a93d3c3cb3da943ff16964193527d
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-03 10:16:16 +0300

    up

--- src/rbus/udp/handle/handleTransmitRBUS.js
+++ src/rbus/udp/handle/handleTransmitRBUS.js
@@ -2,18 +2,19 @@ const { RBUS_MESSAGE_SIZE, RBUS_TTL } = require("../../constants");
 const { getAddress } = require("../../pool");
 
 module.exports.handleTransmitRBUS = (rbus, data) => {
-  console.log(data)
   if (!rbus.port.isRBUS) {
     return;
   }
   if (data.length < 8) {
     return;
   }
-  if (data.length > 7 + RBUS_MESSAGE_SIZE) {
+  if (data.length > 6 + RBUS_MESSAGE_SIZE) {
     return;
   }
   const mac = data.slice(1, 7);
   const address = getAddress(rbus, mac);
+  console.log(data, mac, address)
+
   if (address < 0) {
     return;
   }
