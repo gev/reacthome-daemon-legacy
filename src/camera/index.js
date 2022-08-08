a2281e8918d02b52107a803cfabcd1bf35476839
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-08 19:13:33 +0300

    up

--- src/camera/index.js
+++ src/camera/index.js
@@ -14,7 +14,7 @@ module.exports.onWatch = ({ url, audio = false, video = true }, session) => {
     janus.attachPlugin(session_id, 'janus.plugin.streaming', (handle_id) => {
       bind(handle_id, session);
       const watch = (stream_id) => {
-        janus.send(session_id, handle_id, { request: WATCH, id: stream_id }, ({jsep}) => {
+        janus.send(session_id, handle_id, { request: WATCH, id: stream_id }, ({ jsep }) => {
           if (jsep) {
             send(session, { type: WATCH, url, session_id, handle_id, stream_id, jsep });
           }
@@ -28,18 +28,18 @@ module.exports.onWatch = ({ url, audio = false, video = true }, session) => {
         const rtsp_pwd = u.password;
         u.username = '';
         u.password = '';
-          janus.send(session_id, handle_id, {
-            request: CREATE,
-            id: stream_id,
-            type: RTSP,
-            audio, video,
-            url: u.toString(),
-            rtsp_user, rtsp_pwd,
-            videofmtp: 'level-asymmetry-allowed=1;profile-level-id=42e01f;packetization-mode=1'
-          }, () => {
-            streams.set(url, stream_id);
-            watch(stream_id);
-          });
+        janus.send(session_id, handle_id, {
+          request: CREATE,
+          id: stream_id,
+          type: RTSP,
+          audio, video,
+          url: u.toString(),
+          rtsp_user, rtsp_pwd,
+          // videofmtp: 'level-asymmetry-allowed=1;profile-level-id=42e01f;packetization-mode=1'
+        }, () => {
+          streams.set(url, stream_id);
+          watch(stream_id);
+        });
       }
     })
   });
