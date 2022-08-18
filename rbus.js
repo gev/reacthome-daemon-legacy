8e1358d91c06b3314575011c3bd5c8c88526a9e3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-18 12:04:03 +0300

    yp

--- rbus.js
+++ rbus.js
@@ -5,8 +5,8 @@ const ifaces = os.networkInterfaces();
 const mac = (ifaces.eth0 || ifaces.eth1)[0]
   .mac.split(':').map(i => parseInt(i, 16));
 
-const mac1 = mac;
-const mac2 = mac;
+const mac1 = [...mac];
+const mac2 = [...mac];
 
 mac1[0] = mac1[0] & 0b1111_1110;
 mac2[0] = mac2[0] | 0b0000_0001;
