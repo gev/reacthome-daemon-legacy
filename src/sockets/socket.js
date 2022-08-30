4823c5f8d5d141a535bdf5235f11efe19ca42520
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-08-30 16:11:37 +0300

    cleanup

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -15,56 +15,25 @@ module.exports = (discovery, interval, port, listen, multicast, hasQueue, delay 
     });
   };
 
-  // const send = hasQueue
-  //   ? (packet, ip) => {
-  //     if (!timer[ip]) {
-  //       queue[ip] = [];
-  //       timestamp[ip] = Date.now();
-  //       timer[ip] = setInterval(() => {
-  //         const q = queue[ip];
-  //         if (q.length === 0) return;
-  //         const now = Date.now();
-  //         if (now - timestamp[ip] < delay) return;
-  //         timestamp[ip] = now;
-  //         _send(q.shift(), ip);
-  //       }, 1);
-  //     } else {
-  //       const q = queue[ip];
-  //       if (q.length === 0) {
-  //         const now = Date.now();
-  //         if (now - timestamp[ip] > delay) {
-  //           timestamp[ip] = now;
-  //           _send(packet, ip);
-  //         } else {
-  //           q.push(packet);
-  //         }
-  //       } else {
-  //         q.push(packet);
-  //       }
-  //     }
-  //   }
-  //   : _send;
 
-  // const sendConfirm = (packet, ip, confirm, t = 1000) => {
-  //   if (confirm()) return;
-  //   send(packet, ip);
-  //   setTimeout(sendConfirm, t, packet, ip, confirm, t);
-  // };
+
+  const startDiscovery = () => {
+    try {
+      socket.setMulticastInterface(multicast);
+      setInterval(discovery(socket), interval);
+    } catch (e) {
+      console.error(e);
+      setTimeout(startDiscovery, interval);
+    }
+  };
 
   socket
     .on('error', console.error)
-    .bind(listen, () => {
-      try {
-        socket.setMulticastInterface(multicast);
-        setInterval(discovery(socket), interval);
-      } catch (e) {
-        console.error(e)
-      }
-    });
+    .bind(listen, startDiscovery);
 
-    const handle = (handler) => {
-      socket.on('message', handler);
-    };
+  const handle = (handler) => {
+    socket.on('message', handler);
+  };
 
-    return { handle, send };
+  return { handle, send };
 };
