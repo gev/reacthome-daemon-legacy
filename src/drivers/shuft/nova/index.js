046a58dfb6daf773170678c134b5507a603ddf4f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-09-12 18:57:46 +0300

    up

--- src/drivers/shuft/nova/index.js
+++ src/drivers/shuft/nova/index.js
@@ -66,8 +66,8 @@ module.exports.handle = (action) => {
       switch (data[0]) {
         case READ_INPUT_REGISTERS: {
           const dev = get(id) || {};
-          const value = data.readUInt16BE(2) & 0x1;
-          const fan_speed = data.readUInt16BE(25);
+          const value = data.readUInt16BE(1) & 0x1;
+          const fan_speed = data.readUInt16BE(51);
           // const setpoint = data.readUInt16BE(4) / 10;
           if (dev.synced) {
             set(id, {
