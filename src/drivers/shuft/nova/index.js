598ba2fdb99ed87017a57b0bf878a52c01f34e0e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-09-12 22:12:50 +0300

    up

--- src/drivers/shuft/nova/index.js
+++ src/drivers/shuft/nova/index.js
@@ -26,48 +26,46 @@ const sync = async (id) => {
   const { bind, synced } = dev;
   const [modbus, , address] = bind.split("/");
   if (modbus) {
-    if (synced) {
-      set(id, { sinked: 2 });
-      readInputRegisters(modbus, address, 0x0, 85);
+    readInputRegisters(modbus, address, 0x0, 85);
+    await delay(300);
+    readHoldingRegisters(modbus, address, 0x1f, 1);
+    const { value_, fan_speed_, setpoint_ } = dev
+    if (value_ !== undefined) {
       await delay(300);
-      readHoldingRegisters(modbus, address, 0x1f, 1);
-    } else {
-      const { value_, fan_speed_, setpoint_ } = dev
-      if (value_ !== undefined) {
-        writeRegister(modbus, address, 0x2, value_);
-        set(id, { value_: undefined });
-      }
-      if (fan_speed_ !== undefined) {
-        await delay(300);
-        writeRegister(modbus, address, 0x20, fan_speed_);
-        set(id, { fan_speed_: undefined });
-      }
-      if (setpoint_ !== undefined) {
-        await delay(300);
-        writeRegister(modbus, address, 0x1f, setpoint_ * 10);
-        set(id, { setpoint_: undefined });
-      }
+      writeRegister(modbus, address, 0x2, value_);
+      set(id, { value_: undefined });
+    }
+    if (fan_speed_ !== undefined) {
+      await delay(300);
+      writeRegister(modbus, address, 0x20, fan_speed_);
+      set(id, { fan_speed_: undefined });
+    }
+    if (setpoint_ !== undefined) {
+      await delay(300);
+      writeRegister(modbus, address, 0x1f, setpoint_ * 10);
+      set(id, { setpoint_: undefined });
     }
   }
+}
 };
 
 module.exports.handle = (action) => {
   const { id, type } = action;
   switch (type) {
     case ACTION_ON: {
-      set(id, { value: 1, value_: 1, synced: false });
+      set(id, { value: 1, value_: 1 });
       break;
     }
     case ACTION_OFF: {
-      set(id, { value: 0, value_: 0, synced: false });
+      set(id, { value: 0, value_: 0 });
       break;
     }
     case ACTION_SET_FAN_SPEED: {
-      set(id, { fan_speed: action.value, fan_speed_: action.value, synced: false });
+      set(id, { fan_speed: action.value, fan_speed_: action.value });
       break;
     }
     case ACTION_SETPOINT: {
-      set(id, { setpoint: action.value, setpoint_: action.value, synced: false });
+      set(id, { setpoint: action.value, setpoint_: action.value });
       break;
     }
     default: {
@@ -75,13 +73,13 @@ module.exports.handle = (action) => {
       switch (data[0]) {
         case READ_HOLDING_REGISTERS: {
           const setpoint = data.readUInt16BE(2) / 10;
-          set(id, { setpoint, synced: true });
+          set(id, { setpoint });
           break;
         }
         case READ_INPUT_REGISTERS: {
           const value = data.readUInt16BE(6) & 0x1;
           const fan_speed = data.readUInt16BE(52);
-          set(id, { value, fan_speed, synced: true });
+          set(id, { value, fan_speed });
           break;
         }
       }
