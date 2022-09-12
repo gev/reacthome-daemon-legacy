2ea9967cd47fd7fe3056fe0850f3b9eb6eeb5c05
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-09-12 14:03:38 +0300

    debug

--- src/rbus/serial/handle/rbusTransmitPing.js
+++ src/rbus/serial/handle/rbusTransmitPing.js
@@ -1,6 +1,7 @@
 const { RBUS_PING_PREAMBLE_TRANSMIT } = require("../../constants")
 
 module.exports.rbusTransmitPing = (rbus, address) => {
+  console.log('ping', address)
   rbus.port.sendRBUS([
     RBUS_PING_PREAMBLE_TRANSMIT,
     address
