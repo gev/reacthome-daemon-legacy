52ebf09f12a1109a1b98106d8c5059e05ec9a486
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-10-26 10:53:00 +0300

    up

--- src/drivers/alink/index.js
+++ src/drivers/alink/index.js
@@ -16,7 +16,7 @@ const sync = async (id) => {
   if (modbus && address) {
     if (synced) {
       console.log('Alink read holding')
-      readHoldingRegisters(modbus, 0x1, 0x4001, 11);
+      readHoldingRegisters(modbus, 0x0, 0x4001, 11);
     } else {
       // writeRegister(modbus, address, 0x0, dev.value);
       // await delay(100);
