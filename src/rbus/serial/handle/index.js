a6874ad485dc8bea56ac58d5cfdf3397c96e22e1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-04-22 23:55:46 +0300

    up

--- src/rbus/serial/handle/index.js
+++ src/rbus/serial/handle/index.js
@@ -1,4 +1,4 @@
 module.exports.handle = (rbus) => (data) => {
   console.log("UART receive", data)
-  rbus.socket.send(data.slice(1, data.length - 1))
+  rbus.socket.send(data.slice(2, data.length - 2))
 }
