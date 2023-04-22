c5070431ef7189d3efe296d61127685fcaadeaa8
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-04-23 01:06:18 +0300

    up

--- src/rbus/udp/handle/index.js
+++ src/rbus/udp/handle/index.js
@@ -1,14 +1,16 @@
+const { ACTION_RBUS_TRANSMIT } = require("../../../constants");
+
 module.exports.handle = (rbus) => (data) => {
   console.log("UDP receive", data)
-  // const x = data.slice(2, 8)
-  // const mac = Array.from(x).map(i => i.toString(16)).join(':')
-  // rbus.pool[mac] = { port: data[8], address: data[9] }
-  // a = Array.from(data)
-  // mac = a.slice(0, 6)
-  rbus.port.send([
-    0xa5,
-    data.length,
-    // ...mac,
-    ...data
-  ])
+  if (data[0] = ACTION_RBUS_TRANSMIT) {
+    const x = data.slice(0, 7)
+    mac = Array.from(x.slice(1)).map(i => i.toString(16)).join(':')
+    entry = rbus.pool[mac]
+    if (entry) {
+      const { port, address } = entry
+      rbus.port.send([0xa5, data.length + 2, ...x, port, address, ...data.slice(7)])
+    }
+  } else {
+    rbus.port.send([0xa5, data.length, ...data])
+  }
 }
