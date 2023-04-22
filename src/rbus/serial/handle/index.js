688ad5412331b9134261ec2ecd8407e5ee55c978
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-04-23 00:38:25 +0300

    up

--- src/rbus/serial/handle/index.js
+++ src/rbus/serial/handle/index.js
@@ -1,8 +1,7 @@
 module.exports.handle = (rbus) => (data) => {
   console.log("UART receive", data)
-  const x = data.slice(2)
-  const mac = Array.from(x.slice(0, 6)).map(i => i.toString(16)).join(':')
-  const [port, address] = x.splice(6, 2)
-  rbus.pool[mac] = { port, address }
-  rbus.socket.send(x)
+  const x = data.slice(2, 8)
+  const mac = Array.from(x).map(i => i.toString(16)).join(':')
+  rbus.pool[mac] = { port: data[8], address: data[9] }
+  rbus.socket.send(Buffer.concat([x, data.slice(10)]))
 }
