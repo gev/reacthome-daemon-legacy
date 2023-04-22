a2832e28b4cf7f81de61d3a41258d21f56e1f769
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-04-23 00:42:01 +0300

    up

--- src/rbus/udp/handle/index.js
+++ src/rbus/udp/handle/index.js
@@ -1,11 +1,14 @@
 module.exports.handle = (rbus) => (data) => {
   console.log("UDP receive", data)
-  a = Array.from(data)
-  mac = a.slice(0, 6)
+  // const x = data.slice(2, 8)
+  // const mac = Array.from(x).map(i => i.toString(16)).join(':')
+  // rbus.pool[mac] = { port: data[8], address: data[9] }
+  // a = Array.from(data)
+  // mac = a.slice(0, 6)
   rbus.port.send([
     0xa5,
-    data.length - 2,
-    ...mac,
-    ...data.slice(8)
+    data.length,
+    // ...mac,
+    ...data
   ])
 }
