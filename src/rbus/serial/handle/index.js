203364d9e3edd55ec681236b49fd7ecb01997f55
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-05-02 02:58:19 +0300

    up

--- src/rbus/serial/handle/index.js
+++ src/rbus/serial/handle/index.js
@@ -1,7 +1,89 @@
-module.exports.handle = (rbus) => (data) => {
-  console.log("UART receive", data)
-  const x = data.slice(2, 8)
-  const mac = Array.from(x).map(i => i.toString(16)).join(':')
-  rbus.pool[mac] = { port: data[8], address: data[9] }
-  rbus.socket.send(Buffer.concat([x, data.slice(10, data.length - 2)]))
+const { addCRC } = require('crc');
+
+const WAITING_PREAMBLE = 0
+const WAITING_SIZE = 1
+const WAITING_DATA = 2
+const WAITING_MSB_CRC = 3
+const WAITING_LSB_CRC = 4
+
+const PREAMBLE = 0xa5
+
+module.exports.handle = (rbus) => {
+
+  let phase, offset, size, crc
+  let buff = Buffer.alloc(255)
+
+  const handleRBUS = (buff) => {
+    const x = buff.slice(0, 6)
+    const mac = Array.from(x).map(i => i.toString(16)).join(':')
+    rbus.pool[mac] = { port: buff[6], address: buff[7] }
+    rbus.socket.send(Buffer.concat([x, buff.slice(8)]))
+  }
+
+  const handleRS485 = (buff) => {
+  }
+
+  const receivePreamble = (v) => {
+    if (v === PREAMBLE) {
+      phase = WAITING_SIZE
+      offset = 0
+      size = 0
+      crc = 0
+    }
+  }
+
+  const receiveSize = (v) => {
+    size = v;
+    phase = WAITING_DATA
+  }
+
+  const receiveData = (v) => {
+    buff[offset] = v;
+    offset++
+    if (offset === size) {
+      phase = WAITING_MSB_CRC
+    }
+  }
+
+  const receiveMsbCRC = (v) => {
+    crc = v
+    phase = WAITING_LSB_CRC
+  }
+
+  const receiveLsbCRC = (v) => {
+    crc = (v << 8) | crc
+    if (crc16modbus(buff) = crc) {
+      handleRBUS(buff)
+    }
+    phase = WAITING_PREAMBLE
+  }
+
+  const process = (v) => {
+    switch (phase) {
+      case WAITING_PREAMBLE:
+        receivePreamble(v)
+        break
+      case WAITING_SIZE:
+        receiveSize(v)
+        break
+      case WAITING_DATA:
+        receiveData(v)
+        break
+      case WAITING_MSB_CRC:
+        receiveMsbCRC(v)
+        break
+      case WAITING_LSB_CRC:
+        receiveLsbCRC(v)
+        break
+    }
+  }
+
+  reset()
+
+  return (data) => {
+    console.log("UART receive", data)
+    for (let i = 0; i < data.length; i++) {
+      process(data[i])
+    }
+  }
 }
