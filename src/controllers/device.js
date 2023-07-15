9b41312857425b1e0ce8fad10b1d7505e5c0a43b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-07-15 19:47:04 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -108,7 +108,6 @@ module.exports.manage = () => {
       const dev_mac = Array.from(data.slice(0, 6));
       const id = dev_mac.map((i) => `0${i.toString(16)}`.slice(-2)).join(":");
       const action = data[6];
-      // online(id, { ip: address, ready: true });
       switch (action) {
         case DEVICE_TYPE_PLC: {
           for (let i = 1; i <= 36; i++) {
@@ -705,6 +704,8 @@ module.exports.manage = () => {
         // default: {
         // }
       }
+      online(id, { ip: address, ready: true });
+
     } catch (e) {
       console.error(e);
     }
