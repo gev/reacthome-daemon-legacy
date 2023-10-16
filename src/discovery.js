c0d88389497db710906824176b0234260870c2d8
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-10-16 14:10:01 +0300

    up

--- src/discovery.js
+++ src/discovery.js
@@ -7,14 +7,14 @@ const CLIENT_GROUP = "224.0.0.2";
 const CLIENT_PORT = 2021;
 
 module.exports.start = (id) => {
-  const socket = createSocket("udp4");
-  const discoveryMessage = JSON.stringify({
-    id,
-    type: DISCOVERY,
-    payload: get(id),
-  })
+  const socket = createSocket({ type: "udp4", reuseAddr: true, reusePort: true });
   socket.on("error", console.error);
   setInterval(() => {
+    const discoveryMessage = JSON.stringify({
+      id,
+      type: DISCOVERY,
+      payload: get(id),
+    })
     socket.send(discoveryMessage, CLIENT_PORT, CLIENT_GROUP);
   }, 10_000)
 };
