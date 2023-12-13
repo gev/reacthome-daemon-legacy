4587808f3245d380e3a6bdcda5ebe825365e357b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-12-13 22:23:16 +0300

    up

--- rbus2.js
+++ rbus2.js
@@ -1,5 +1,5 @@
 const os = require('os');
-const { rbus } = require("./src/rbus");
+const { rbus } = require("./src/rbus/v2");
 
 const ifaces = os.networkInterfaces();
 const mac = (ifaces.eth0 || ifaces.eth1)[0]
