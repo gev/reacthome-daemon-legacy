1a03842b4fe253321e9800119165bc8ed6a5eb58
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-03-29 15:24:17 +0300

    clean

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -644,7 +644,6 @@ module.exports.manage = () => {
         }
         case ACTION_DOPPLER1: {
           const value = [...data.slice(7)];
-          console.log(value);
           const { onDoppler } = get(id) || {};
           set(id, { value });
           if (onDoppler) {
