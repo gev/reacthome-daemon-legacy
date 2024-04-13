642a1c6a7299d269301138f2fd12a0873580d7e1
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-04-14 01:41:49 +0300

    up

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -204,6 +204,7 @@ const bind = ["r", "g", "b", "bind"];
 const rgb = ["r", "g", "b"];
 
 const run = (action) => {
+  console.log(action)
   try {
     switch (action.type) {
       case ACTION_SET: {
