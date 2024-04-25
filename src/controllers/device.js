34989ed97761805730c9a205b145e8894c729114
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-04-26 00:24:35 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -555,6 +555,7 @@ module.exports.manage = () => {
             case DEVICE_TYPE_SMART_TOP_A6P:
             case DEVICE_TYPE_SMART_TOP_G4D: {
               const image = Array.from(data.slice(7, 15))
+              console.log(data, id, image)
               set(id, { image });
               break;
             }
