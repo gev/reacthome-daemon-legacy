e513f36a8a2ecb45d9068b8559b802a67dc25333
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-05-17 18:03:43 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -976,21 +976,21 @@ const renderSmartTop = (id) => {
       case 'MODE_HEAT':
         value = typeof temperature === 'number' ? temperature.toFixed(1) : "";
         run({ type: ACTION_PRINT, id, image, value });
-        console.log(current_mode, value)
+        console.log(current.mode, value)
         break;
       case 'MODE_WET':
         value = typeof humidity === 'number' ? humidity.toFixed(1) : "";
         run({ type: ACTION_PRINT, id, image, value });
-        console.log(current_mode, value)
+        console.log(current.mode, value)
         break;
       case 'MODE_VENTILATION':
         value = typeof co2 === 'number' ? co2.toFixed(1) : "";
         run({ type: ACTION_PRINT, id, image, value });
-        console.log(current_mode, value)
+        console.log(current.mode, value)
         break;
       default:
         run({ type: ACTION_PRINT, id, value: "" })
-        console.log(current_mode)
+        console.log(current.mode)
     }
   } else {
     run({ type: ACTION_IMAGE, id, value: image })
