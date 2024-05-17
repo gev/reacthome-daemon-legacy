cf9bbce6028a50c01b2be9d45e940b1738444fd4
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-05-17 18:11:57 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -984,7 +984,7 @@ const renderSmartTop = (id) => {
         console.log(current.mode, value)
         break;
       case 'MODE_VENTILATION':
-        value = typeof co2 === 'number' ? Math.round(co2) : "";
+        value = typeof co2 === 'number' ? co2.toFixed(0) : "";
         run({ type: ACTION_PRINT, id, image, value });
         console.log(current.mode, value)
         break;
