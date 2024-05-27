cb1f66e76a75c672d28019e9a16c2d3778e73505
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-05-27 11:44:40 +0300

    up

--- src/drivers/modbus/index.js
+++ src/drivers/modbus/index.js
@@ -1,22 +1,36 @@
 const instances = require("../drivers");
 
-module.exports.readCoils = (id, address, register, data) =>
-    instances.get(id).readCoils(id, address, register, data);
+module.exports.readCoils = (id, address, register, data) => {
+    const instance = instances.get(id);
+    if (instance) instance.readCoils(id, address, register, data);
+}
 
-module.exports.readInputs = (id, address, register, data) =>
-    instances.get(id).readInputs(id, address, register, data);
+module.exports.readInputs = (id, address, register, data) => {
+    const instance = instances.get(id);
+    if (instance) instance.readInputs(id, address, register, data);
+}
 
-module.exports.readHoldingRegisters = (id, address, register, data) =>
-    instances.get(id).readHoldingRegisters(id, address, register, data);
+module.exports.readHoldingRegisters = (id, address, register, data) => {
+    const instance = instances.get(id);
+    if (instance) instance.readHoldingRegisters(id, address, register, data);
+}
 
-module.exports.readInputRegisters = (id, address, register, data) =>
-    instances.get(id).readInputRegisters(id, address, register, data);
+module.exports.readInputRegisters = (id, address, register, data) => {
+    const instance = instances.get(id);
+    if (instance) instance.readInputRegisters(id, address, register, data);
+}
 
-module.exports.writeCoil = (id, address, register, data) =>
-    instances.get(id).writeCoil(id, address, register, data);
+module.exports.writeCoil = (id, address, register, data) => {
+    const instance = instances.get(id);
+    if (instance) instance.writeCoil(id, address, register, data);
+}
 
-module.exports.writeRegister = (id, address, register, data) =>
-    instances.get(id).writeRegister(id, address, register, data);
+module.exports.writeRegister = (id, address, register, data) => {
+    const instance = instances.get(id);
+    if (instance) instance.writeRegister(id, address, register, data);
+}
 
-module.exports.writeRegisters = (id, address, register, data) =>
-    instances.get(id).writeRegisters(id, address, register, data);
+module.exports.writeRegisters = (id, address, register, data) => {
+    const instance = instances.get(id);
+    if (instance) instance.writeRegisters(id, address, register, data);
+}
