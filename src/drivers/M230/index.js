e3ea4978ddfd7bb26be165b3f5b8372fe69d3094
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-06-14 13:53:07 +0300

    up

--- src/drivers/M230/index.js
+++ src/drivers/M230/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const SerialPort = require('serialport');
 const crc = require('crc').crc16modbus;
-const { set } = require('../../actions');
+const { get, set } = require('../../actions');
+const { device } = require('../../sockets');
+const { ACTION_RS485_TRANSMIT, DEVICE_TYPE_RS_HUB1_RS } = require('../../constants');
 
 const address = 0x46;
-const device = '/dev/ttyUSB0';
+// const device = '/dev/ttyUSB0';
 const delay = 200;
 const period = 15000;
 
@@ -17,22 +18,21 @@ module.exports = class {
   }
 
   start() {
-    this.socket = new SerialPort(device, {
-      baudRate: 9600,
-      dataBits: 8,
-      parity: 'none',
-      stopBits: 1,
-      autoOpen: true
-    });
-    this.socket
-        .on('data', this.process)
-        .on('error', console.error);
+    // this.socket = new SerialPort(device, {
+    //   baudRate: 9600,
+    //   dataBits: 8,
+    //   parity: 'none',
+    //   stopBits: 1,
+    //   autoOpen: true
+    // });
+    // this.socket
+    //     .on('data', this.process)
+    //     .on('error', console.error);
     this.request();
   }
 
   stop() {
     clearTimeout(this.t);
-    this.socket.close();
   }
 
   login = () => {
@@ -44,7 +44,7 @@ module.exports = class {
   };
 
   pool = [];
-  process = data => {
+  handle = ({ id, data }) => {
     clearTimeout(this.t);
     this.pool.push(data);
     this.t = setTimeout(() => {
@@ -60,22 +60,49 @@ module.exports = class {
         for (let i = 0; i < 6; i++) {
           v[i] = ((buff.readUInt16LE(i * 16 + 1) << 16) | buff.readUInt16LE(i * 16 + 3)) / 100;
         }
-        set(this.id, { value: [v[0], v[1]], total: v[4] });
+        set(id, { value: [v[0], v[1]], total: v[4] });
         this.t = setTimeout(this.request, period);
       }
     }, delay);
   };
 
+  // send = (cmd) => {
+  //   this.socket.write(this.query(cmd), err => {
+  //     if (err) console.error('error');
+  //   });
+  // }
+
   send = (cmd) => {
-    this.socket.write(this.query(cmd), err => {
-      if (err) console.error('error');
-    });
-  }
+    const { bind } = get(this.id);
+    if (!bind) return;
+    const { is_rbus } = get(bind);
+    if (is_rbus) return;
+    const [dev, , index] = bind.split('/');
+    const { ip, type } = get(dev);
+    const header = Buffer.from([ACTION_RS485_TRANSMIT, index]);
+    const payload = Buffer.alloc(5, 0);
+    payload.writeUInt32BE(address, 0);
+    payload.writeUInt8(cmd, 4);
+    const crc = Buffer.alloc(2);
+    crc.writeUInt16LE(crc16(payload), 0);
+    const buffer = Buffer.concat([header, payload, crc]);
+    switch (type) {
+      case DEVICE_TYPE_RS_HUB1_RS: {
+        device.sendRBUS(buffer, dev);
+        break;
+      }
+      default: {
+        device.send(buffer, ip);
+      }
+    }
+
+  };
+
 
   query = (cmd) => {
     const buff = Buffer.alloc(1 + cmd.length);
     buff.writeUInt8(address & 0xff, 0);
-    cmd.forEach((b, i) => {buff.writeUInt8(b, i + 1)});
+    cmd.forEach((b, i) => { buff.writeUInt8(b, i + 1) });
     const req = Buffer.alloc(buff.length + 2, buff);
     req.writeUInt16LE(crc(buff), buff.length);
     return req;
