2dec4b36c9fc8b8cab8cae1e472a2ecac0cc1556
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-07-30 12:44:36 +0300

    up

--- src/drivers/dlc-02/index.js
+++ src/drivers/dlc-02/index.js
@@ -12,8 +12,8 @@ const sync = async (id, kind, modbus, address, port, n, mask) => {
     const { synced, value } = get(ch) || {};
     if (!synced) {
       addr = (port << 8) | (mask | i)
-      writeRegisters(modbus, address, 41001, [addr, (1 << 8) | (value ? 1 : 0), 0, 0]);
-      await delay(30);
+      // writeRegisters(modbus, address, 41001, [addr, (1 << 8) | (value ? 1 : 0), 0, 0]);
+      // await delay(30);
       writeRegisters(modbus, address, 41001, [addr, (2 << 8) | value, 0, 0]);
       set(ch, { synced: true });
       await delay(50);
