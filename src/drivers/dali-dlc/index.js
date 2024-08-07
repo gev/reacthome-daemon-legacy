5c29b0cbb393b4adbd9d814a6113e67ac704067e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-08-07 21:21:11 +0300

    up

--- src/drivers/dali-dlc/index.js
+++ src/drivers/dali-dlc/index.js
@@ -16,7 +16,7 @@ const sync = async (id, kind, modbus, address, port, n, mask) => {
       writeRegisters(modbus, address, 41001, [(port << 8) | (mask | i), (2 << 8) | value, 0, 0]);
       set(ch, { synced: true });
     } else {
-      readWriteRegisters(modbus, address, 32001, 4, 42001, [(tid << 8) | port, ((mask | i) << 8) | 1]);
+      // readWriteRegisters(modbus, address, 32001, 4, 42001, [(tid << 8) | port, ((mask | i) << 8) | 1]);
       tid += 1;
       tid %= 0xff;
     }
