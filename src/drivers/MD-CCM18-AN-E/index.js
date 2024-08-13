d82877e626fed95799a55f78a8ce3fa82772055e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-08-13 17:09:15 +0300

    up

--- src/drivers/MD-CCM18-AN-E/index.js
+++ src/drivers/MD-CCM18-AN-E/index.js
@@ -1,41 +1,45 @@
 
 const { get, set } = require('../../actions');
 const { ACTION_SET_ADDRESS, ACTION_SET_FAN_SPEED, ACTION_ON, ACTION_OFF, ACTION_SET_MODE, ACTION_SET_DIRECTION, ACTION_SETPOINT } = require('../../constants');
-const { writeRegister, readHoldingRegisters, writeRegisters } = require('../modbus');
-const { READ_HOLDING_REGISTERS, WRITE_REGISTER } = require('../modbus/constants');
+const { writeRegister, readHoldingRegisters, writeRegisters, readCoils } = require('../modbus');
+const { READ_HOLDING_REGISTERS, WRITE_REGISTER, READ_COILS } = require('../modbus/constants');
 const { BROADCAST_ADDRESS, TIMEOUT } = require('./constants');
 const { del } = require('../../db');
 const { delay } = require('../../util');
 
 const instance = new Map();
 
+let index = 0
+
 const sync = async (id, modbus, address, n) => {
-  for (let i = 1; i <= n; i += 1) {
-    const ch = `${id}/${ac}/${i}`
-    const { synced, value } = get(ch) || {};
+  for (let i = 0; i < n; i += 1) {
+    const ch = `${id}/${ac}/${i + 1}`
+    const { synced, value, mode, fan_speed, setpoint } = get(ch) || {};
     if (!synced) {
-      writeRegister(modbus, address, r + i * 5, value > 254 ? 254 : value);
+      const dataMode = (value ? 1 : 0) << 7 | 1 << mode;
+      let dataFan = 0;
+      switch (fan_speed) {
+        case 0:
+          dataFan = 0b1000_0000;
+          break;
+        case 1:
+          dataFan = 0b0000_0001;
+          break;
+        case 2:
+          dataFan = 0b0000_0010;
+          break;
+        case 3:
+          dataFan = 0b0000_0100;
+          break;
+      }
+      writeRegisters(modbus, address, 40002 + i * 32, [dataMode, dataFan, setpoint]);
       set(ch, { synced: true });
-      await delay(50);
+    } else {
+      index = i + 1;
+      readCoils(modbus, address, i * 128, 16);
     }
+    await delay(100);
   }
-  // const dev = get(id) || {};
-  // const { bind, synced } = dev;
-  // const [modbus, , address] = bind.split('/');
-  // if (synced) {
-  //   readHoldingRegisters(modbus, address, 0x0, 12);
-  // } else {
-  //   writeRegister(modbus, address, 0x0, dev.value);
-  //   await delay(100);
-  //   writeRegister(modbus, address, 0x1, dev.mode);
-  //   await delay(100);
-  //   writeRegister(modbus, address, 0x2, dev.fan_speed);
-  //   await delay(100);
-  //   writeRegister(modbus, address, 0x3, dev.direction);
-  //   await delay(100);
-  //   writeRegister(modbus, address, 0x4, dev.setpoint);
-  //   set(id, { synced: true });
-  // }
 };
 
 
@@ -80,22 +84,39 @@ module.exports.run = (action) => {
 
 module.exports.handle = (action) => {
   const { id, data } = action;
-  switch (data[0]) {
-    case READ_HOLDING_REGISTERS: {
-      const dev = get(id) || {};
-      if (dev.synced) {
-        set(id, {
-          value: data.readUInt16BE(2),
-          mode: data.readUInt16BE(4),
-          fan_speed: data.readUInt16BE(6),
-          direction: data.readUInt16BE(8),
-          setpoint: data.readUInt16BE(10),
-          synced: true
-        })
-      }
+  console.log(data);
+  let value = data[0] >> 7;
+  let mode = 4;
+  switch (data[0] & 0b11111) {
+    case 0b00001:
+      mode = 0;
       break;
-    }
+    case 0b0010:
+      mode = 1;
+      break;
+    case 0b00100:
+      mode = 2;
+      break;
+    case 0b01000:
+      mode = 3;
+      break;
+    case 0b10000:
+      mode = 4;
+      break;
   }
+  let fan_speed = 0;
+  switch (data[1] & 0b111) {
+    case 0b001:
+      fan_speed = 1;
+      break;
+    case 0b010:
+      fan_speed = 2;
+      break;
+    case 0b100:
+      fan_speed = 3;
+      break;
+  }
+  set(`${id}/ac/${index}`, id, { value, mode, fan_speed });
 };
 
 
@@ -109,5 +130,3 @@ module.exports.add = (id) => {
   }
   instance.set(id, setTimeout(loop(id), 100));
 };
-
-let index = 0;
