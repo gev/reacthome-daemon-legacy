2f3dd5a773ee9fdb376a57b0054690a68930fe6f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-08-15 15:27:51 +0300

    up

--- src/drivers/MD-CCM18-AN-E/index.js
+++ src/drivers/MD-CCM18-AN-E/index.js
@@ -36,7 +36,7 @@ const sync = async (id, modbus, address, n) => {
       set(ch, { synced: true });
     } else {
       index = i + 1;
-      readCoils(modbus, address, 10001 + i * 128, 128);
+      readCoils(modbus, address, 1 + i * 128, 128);
     }
     await delay(1000);
   }
