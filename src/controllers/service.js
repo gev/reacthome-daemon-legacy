8d677333b6bbb9a96b0679dfcc2ee4cacde95481
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-10-22 15:39:59 +0300

    up

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -2769,7 +2769,6 @@ const run = (action) => {
             action.inverseDirection,
             ...action.params || []
           ]);
-          console.log(buff);
           switch (dev.type) {
             case DEVICE_TYPE_DI_4:
             case DEVICE_TYPE_DOPPLER_1_DI_4:
