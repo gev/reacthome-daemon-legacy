7e73830e80dfc024cb81ee183e9931621c140bfe
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-11-11 14:49:03 +0300

    up

--- src/rbus/udp/create.js
+++ src/rbus/udp/create.js
@@ -23,7 +23,7 @@ module.exports.createSocket = (rbus, host) => {
       ...rbus.mac,
       rbus.ready ? ACTION_READY : ACTION_DISCOVERY,
       DEVICE_TYPE_SERVER,
-      5, 0 // Version
+      4, 0 // Version
     ]))
   }, 1_000)
   rbus.socket.send(Buffer.from([...rbus.mac, ACTION_INITIALIZE]));
