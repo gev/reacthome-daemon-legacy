62e365cdf1ce65ed34949bdb9ee0601840dcffef
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-12-18 17:13:27 +0300

    up

--- src/rbus/serial/create.js
+++ src/rbus/serial/create.js
@@ -11,8 +11,9 @@ const createPort = (rbus, path) => {
   )
   port.on('data', handle(rbus));
   const send = (data) => {
-    console.log("UART send", data)
-    port.write(data)
+    const buff = Buffer.from(data);
+    console.log("UART send", buff)
+    port.write(buff)
   }
   rbus.port = {
     path,
