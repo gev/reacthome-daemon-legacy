8b569b17fd5816c28ea61b8f64a21893fa326954
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-04-25 13:29:33 +0300

    up

--- src/assist/lang/ru.js
+++ src/assist/lang/ru.js
@@ -3,10 +3,10 @@ const sqlite = require('better-sqlite3');
 const db = sqlite('./var/lang/ru.db');
 
 const query = db.prepare(`
-    SELECT DISTINCT forms.text
-    FROM forms
-    JOIN lemmas ON forms.lemma = lemmas.id
-    WHERE lemmas.text = ?
+    SELECT DISTINCT f1.text
+    FROM forms f1
+    JOIN forms f2 ON f1.lemma = f2.lemma
+    WHERE f2.text = ?
 `)
 
 const getAllForms = (lemma) => query.all(lemma.toLowerCase()).map(row => row.text)
