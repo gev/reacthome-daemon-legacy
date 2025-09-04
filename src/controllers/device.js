bbd6d25dbbac86b53aa4676421ae62f23bb49e7f
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-09-04 15:55:08 +0300

    up

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -940,10 +940,10 @@ module.exports.manage = () => {
         }
         case ACTION_LOG: {
           const { type } = get(id) || {};
-          console.log(data);
+          const value = [...data.slice(7)];
+          console.log(value);
           switch (type) {
             case DEVICE_TYPE_SMART_TOP_G6: {
-              const value = [...data.slice(7)];
               // console.log(value);
                 set(id, { value });
                 break;
