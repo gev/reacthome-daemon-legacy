ccc5c55ef98d861dbf99c9f9644fdd6c07fcfccd
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-10-06 13:53:21 +0300

    Revert "create action log"
    This reverts commit 8c54dc6efd90c2af6ed60178063f11d50f1a8231.

--- src/controllers/device.js
+++ src/controllers/device.js
@@ -104,7 +104,6 @@ const {
   DEVICE_TYPE_SMART_TOP_A4TD,
   DEVICE_TYPE_LANAMP,
   DEVICE_TYPE_SOUNDBOX,
-  ACTION_LOG,
 } = require("../constants");
 const {
   get,
@@ -936,17 +935,6 @@ module.exports.manage = () => {
           }
           set(`${id}/LA/${index}`, { colors, segments });
           break;
-        }
-        case ACTION_LOG: {
-          const { type } = get(id) || {};
-          switch (type) {
-            case DEVICE_TYPE_SMART_TOP_G6: {
-                const value = [...data.slice(8)];
-                set(id, { value });
-                break;
-              }
-          }
-          
         }
         case ACTION_ERROR: {
           const reason = data[7];
