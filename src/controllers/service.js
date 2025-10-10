35b0f6d0f14aeaa5292c8b57cd58f7bd8732c214
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-10-11 00:35:24 +0300

    up

--- src/controllers/service.js
+++ src/controllers/service.js
@@ -1352,9 +1352,9 @@ const run = (action) => {
               const mask = dict[c] || 0;
               const offset = offsets[i];
               if (i === 3) {
-                setBit(offset[0], (mask >> 4) & 1);
-                setBit(offset[1], (mask >> 2) & 1);
-                setBit(offset[2], (mask >> 0) & 1);
+                // setBit(offset[0], (mask >> 4) & 1);
+                // setBit(offset[1], (mask >> 2) & 1);
+                // setBit(offset[2], (mask >> 0) & 1);
               } else {
                 for (let k = 0; k < 7; k++) {
                   setBit(offset[k], (mask >> (6 - k)) & 1);
@@ -1383,7 +1383,6 @@ const run = (action) => {
                 setBit(7, 1);
                 setBit(8, 1);
             }
-            console.log(image);
             run({ type: ACTION_IMAGE, id, value: image })
             break;
           }
