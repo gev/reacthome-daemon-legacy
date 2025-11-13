7b360dc3ad08e87ea7814fe3f834d454713ea002
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-11-13 16:36:43 +0300

    up

--- src/drivers/dauerhaft/index.js
+++ src/drivers/dauerhaft/index.js
@@ -175,6 +175,7 @@ module.exports.run = (action) => {
 
 module.exports.handle = ({ id, data }) => {
   const ch = indexes.get(id);
+  console.log("Resived", ch, data);
   switch (data[0]) {
     case 0xd8: {
       if (ch) {
