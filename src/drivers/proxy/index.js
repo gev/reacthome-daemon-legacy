0a84b34c24c4f25f446f414e33fef6756115eb9c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-11-30 22:47:10 +0300

    proxy

--- src/drivers/proxy/index.js
+++ src/drivers/proxy/index.js
@@ -1,18 +1,67 @@
 
 
 const { get, set } = require('../../actions');
+const { HYGROSTAT, DRIVER_TYPE_DAUERHAFT } = require('../../constants');
 
 const timers = new Map();
 
-const loop = (id) => () => {
-  proxy = get(id) || {};
-  source = get(proxy.bind) || {};
-  target = get(proxy.proxy) || {};
-  console.log('Proxy driver loop', id, proxy);
-  console.log('Bind', source);
-  console.log('Proxy', target);
+const sync = (id, i) => {
+  console.log('Sync', id, i);
+  const ch = `${id}/proxy/${i}`;
+  console.log('Channel', ch);
+  const proxy = get(ch) || {};
+  console.log('Proxy', proxy);
+  if (!proxy.proxy || !proxy.bind) return;
+  const source = get(proxy.bind) || {};
+  const target = get(proxy.proxy) || {};
+  console.log('Source', source);
+  console.log('Target', target);
+  switch (target.type) {
+    case HYGROSTAT:
+      syncHygrostat(ch, proxy, source, target);
+      break;
+    default:
+      const segments = proxy.proxy.split('/');
+      const dev = get(segments[0]) || {};
+      switch (dev.type) {
+        case DRIVER_TYPE_DAUERHAFT:
+          syncCurtains(ch, proxy, source, target);
+          break;
+      }
+  }
 }
 
+const syncHygrostat = (ch, proxy, source, target) => {
+  if (source.timestamp > target.timestamp) {
+    if (!source.last) return;
+    const value = source.last.value;
+    set(ch, { value })
+    set(proxy.proxy, { setpoint: value / 2.55 })
+  } else {
+    const value = target.setpoint * 2.55;
+    set(ch, { value })
+  }
+}
+
+const syncCurtains = (ch, proxy, source, target) => {
+  if (source.timestamp > target.timestamp) {
+    if (!source.last) return;
+    const value = source.last.value;
+    set(ch, { value })
+    set(proxy.proxy, { position: value })
+  } else {
+    const value = target.value;
+    set(ch, { value })
+  }
+}
+
+const loop = (id) => () => {
+  const driver = get(id) || {};
+  console.log('Driver', driver);
+  for (let i = 0; i < driver.numberProxy; i++) {
+    sync(id, i + 1);
+  }
+}
 
 module.exports.run = (action) => {
   console.log('Run', action);
@@ -21,7 +70,6 @@ module.exports.run = (action) => {
 module.exports.handle = (action) => {
   console.log('Handle', action);
 }
-
 
 module.exports.clear = () => {
   for (const i of timers.values()) {
@@ -34,5 +82,5 @@ module.exports.add = (id) => {
   if (timers.has(id)) {
     clearInterval(timers.get(id))
   }
-  timers.set(id, setInterval(loop(id), 5000));
+  timers.set(id, setInterval(loop(id), 1000));
 };
