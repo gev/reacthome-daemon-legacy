cfa864ff156658817903cdf13b082c98b4abb673
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-01-18 23:58:22 +0300

    up

--- src/drivers/proxy/index.js
+++ src/drivers/proxy/index.js
@@ -9,6 +9,8 @@ const sync = (id, index) => {
   if (!proxy.proxy || !proxy.bind) return;
   const source = get(proxy.bind) || {};
   const target = get(proxy.proxy) || {};
+  console.log('source', source);
+  console.log('target', target);
   switch (target.type) {
     case HYGROSTAT:
       syncHygrostat(ch, source, target);
