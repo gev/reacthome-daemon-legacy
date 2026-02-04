cb5d634d54a48a32544fbc84dc4204c4eb247c87
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-02-04 18:44:02 +0300

    up

--- src/rbus/udp/create.js
+++ src/rbus/udp/create.js
@@ -9,6 +9,7 @@ module.exports.createSocket = (rbus, host) => {
   const send = (data) => {
     // console.log("UDP send", data)
     if (rbus.mac) {
+      console.log(rbus, data)
       socket.send(
         Buffer.concat([rbus.mac, data]),
         DEVICE_SERVER_PORT,
