e84556914bfa8bb582024dea70d96be5bc282d1e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-02-27 12:31:45 +0300

    up

--- src/sockets/device.js
+++ src/sockets/device.js
@@ -20,10 +20,7 @@ const device = socket((socket) => {
   data.writeUInt32BE(IP_ADDRESS, 1);
   data.writeUInt16BE(socket.address().port, 5);
   return () => {
-    push(() => {
-      // console.log("send discovery", data);
-      device.send(data, DEVICE_GROUP);
-    });
+    device.send(data, DEVICE_GROUP);
   };
 }, DISCOVERY_INTERVAL, DEVICE_PORT, DEVICE_SERVER_PORT, '172.16.0.1');
 
