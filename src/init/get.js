3affaf96a8b71efd82a4f8ce37f016e9106ee0e4
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-02-27 17:15:57 +0300

    dont send assets

--- src/init/get.js
+++ src/init/get.js
@@ -7,17 +7,17 @@ module.exports = async ({ state = [], assets = [] }, session) => {
   for (const id of state) {
     send(session, { type: ACTION_SET, id, payload: get(id) });
   }
-  for await (const name of assets) {
-    try {
-      if (typeof name !== 'string') continue;
-      const file = asset(name);
-      if (await exists(file)) {
-        const data = await readFile(file);
-        const payload = data.toString('base64');
-        send(session, { type: ACTION_ASSET, name, payload });
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }
+  // for await (const name of assets) {
+  //   try {
+  //     if (typeof name !== 'string') continue;
+  //     const file = asset(name);
+  //     if (await exists(file)) {
+  //       const data = await readFile(file);
+  //       const payload = data.toString('base64');
+  //       send(session, { type: ACTION_ASSET, name, payload });
+  //     }
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // }
 };
