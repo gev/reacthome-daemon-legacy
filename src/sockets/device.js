d4fbb778a4d69eaf3cc16b79b34d222152b4041e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-03-03 21:33:32 +0300

    кумуке йгугу

--- src/sockets/device.js
+++ src/sockets/device.js
@@ -26,40 +26,40 @@ const device = socket((socket) => {
 
 
 device.sendRBUS = (data, id) => {
-  // push(() => {
-  const mac = id.split(":").map((i) => parseInt(i, 16));
-  const header = [ACTION_RBUS_TRANSMIT, ...mac];
-  const dev = get(id);
-  let buff;
-  if (dev) {
-    if (dev.hub) {
-      buff = Buffer.from([...header, dev.port, dev.address, ...data]);
-      // console.log("send rbus via hub", buff);
-    } else {
-      buff = Buffer.from([...header, ...data]);
+  push(() => {
+    const mac = id.split(":").map((i) => parseInt(i, 16));
+    const header = [ACTION_RBUS_TRANSMIT, ...mac];
+    const dev = get(id);
+    let buff;
+    if (dev) {
+      if (dev.hub) {
+        buff = Buffer.from([...header, dev.port, dev.address, ...data]);
+        // console.log("send rbus via hub", buff);
+      } else {
+        buff = Buffer.from([...header, ...data]);
+      }
+      device.send(buff, dev.ip);
     }
-    device.send(buff, dev.ip);
-  }
-  // });
+  });
 }
 
 
 device.sendTOP = (data, id) => {
-  // push(() => {
-  const { bottom } = get(id) || {};
-  if (bottom) {
-    // console.log("send top", data);
-    device.sendRBUS([ACTION_SMART_TOP, ...data], bottom);
-  }
-  // });
+  push(() => {
+    const { bottom } = get(id) || {};
+    if (bottom) {
+      // console.log("send top", data);
+      device.sendRBUS([ACTION_SMART_TOP, ...data], bottom);
+    }
+  });
 }
 
-// setInterval(() => {
-//   const run = queue.shift();
-//   if (run) {
-//     run();
-//   }
-// }, 10)
+setInterval(() => {
+  const run = queue.shift();
+  if (run) {
+    run();
+  }
+}, 10)
 
 const push = (run) => {
   queue.push(run);
