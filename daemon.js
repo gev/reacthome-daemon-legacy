dc4c130628a5e22f51770dc432d2a33c55265a87
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-04-23 18:23:38 +0300

    up

--- daemon.js
+++ daemon.js
@@ -90,9 +90,13 @@ const initFirmware = (id) => {
     const mcuName = header.substring(15)
     
     const key = `${deviceType}_${boardVersion}_${dfuMajorVersion}_${mcuName}`;
+    
+    const firmwareMajorVersion = parseInt(header.substring(7,9), 16);
+    const firmwareMinorVersion = parseInt(header.substring(9,11), 16);
+    const version = `${firmwareMajorVersion},${firmwareMinorVersion}`,
 
     const list = firmwares[key] || [];
-    firmwares[key] = [...list, file];
+    firmwares[key] = [...list, {file, version }];
   }
   set(id, {firmwares});
 }
