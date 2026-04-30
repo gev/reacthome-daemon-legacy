f92cd0b801e4893d2b2c1952446bbd526be59f33
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-04-30 14:14:31 +0300

    Create firmwares.js

---
+++ src/firmwares.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const firmwares = {};
+
+const folderFirmware = "firmware";
+
+const initFirmware = (id) => {
+  const files = fs.readdirSync(folderFirmware);
+  for (const firmware of files) {
+    const header = fs
+      .readFileSync(`${folderFirmware}/${firmware}`, "utf8")
+      .split("\n")[0];
+
+    const deviceType = parseInt(header.substring(1, 5), 16);
+    const boardVersion = parseInt(header.substring(5, 7), 16);
+    const dfuMajorVersion = parseInt(header.substring(11, 13), 16);
+    const mcuName = header.substring(15);
+
+    const key = `${deviceType}_${boardVersion}_${dfuMajorVersion}_${mcuName}`;
+
+    const firmwareMajorVersion = parseInt(header.substring(7, 9), 16);
+    const firmwareMinorVersion = parseInt(header.substring(9, 11), 16);
+    const version = `${firmwareMajorVersion}.${firmwareMinorVersion}`;
+
+    const list = firmwares[key] || [];
+    firmwares[key] = [...list, { firmware, version }];
+  }
+  set(id, { firmwares });
+};
+
+module.exports.initFirmware = initFirmware;
+module.exports.firmwares = firmwares;
