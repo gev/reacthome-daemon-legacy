c70e75599f7567c191223a5186bb051ee8691187
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-06-01 19:03:50 +0300

    up

--- src/discovery.js
+++ src/discovery.js
@@ -15,25 +15,26 @@ const discovery = async (id, ip) => {
   if (!ip) return;
   const socket = createSocket({ type: "udp4", reuseAddr: true, reusePort: true });
   socket.on("error", console.error);
+
   const discoveryMessage = JSON.stringify({
     id,
     type: DISCOVERY,
     payload: get(id),
   })
 
-  socket.bind(0, () => {
-    socket.setMulticastInterface(ip);
-  })
-
-  await socket.send(discoveryMessage, CLIENT_PORT, CLIENT_GROUP);
-
   const data = Buffer.alloc(7);
   data.writeUInt8(ACTION_DISCOVERY, 0);
   data.writeUInt32BE(ip2int(ip), 1);
   data.writeUInt16BE(socket.address().port, 5);
-  await socket.send(data, DEVICE_PORT, DEVICE_GROUP);
 
-  socket.close()
+  socket.bind(ip, () => {
+    socket.setMulticastInterface(ip);
+    await socket.send(discoveryMessage, CLIENT_PORT, CLIENT_GROUP);
+    await socket.send(data, DEVICE_PORT, DEVICE_GROUP);
+    socket.close()
+  })
+
+
 }
 
 const getIP = (name) => {
