2242573285917b25f71e879acbe4e5e2d66527f7
Evgeny Gazdovsky <gazdovsky@gmail.com>
2026-07-10 16:13:51 +0300

    merge

--- rbus.js
+++ rbus.js
@@ -1,5 +1,10 @@
+const os = require('os');
 const { rbus } = require("./src/rbus");
 
+
 setTimeout(() => {
-  rbus('127.0.1.1', '/dev/ttyAMA0');
-}, 3_000);
+  const ifaces = os.networkInterfaces();
+  const mac = (ifaces.eth0 || ifaces.eth1)[0]
+    .mac.split(':').map(i => parseInt(i, 16));
+  rbus(mac, '127.0.1.1', '/dev/ttyAMA0')
+}, 10_000)
