604e9a1d1366a1e114f06d02a04f7ba9b0d27579
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-09-11 18:25:04 +0300

    up

--- src/drivers/HCPC-H2M1C/index.js
+++ src/drivers/HCPC-H2M1C/index.js
@@ -91,7 +91,7 @@ module.exports.run = (action) => {
       break;
     }
     case ACTION_SETPOINT: {
-      set(ch, { setpoint: Math.max(17, Math.min(30, action.value)), synced: false });
+      set(ch, { setpoint: Math.max(16, Math.min(32, action.value)), synced: false });
       break;
     }
   }
