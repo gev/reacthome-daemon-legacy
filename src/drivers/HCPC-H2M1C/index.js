116b1c70987fb61845688cf99facd8240e6d397f
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-09-11 17:52:08 +0300

    up

--- src/drivers/HCPC-H2M1C/index.js
+++ src/drivers/HCPC-H2M1C/index.js
@@ -15,6 +15,7 @@ const sync = async (id, modbus, address, n) => {
     const { synced, value, mode, fan_speed, setpoint } = get(ch) || {};
     if (!synced) {
       const dataMode = 1 << mode;
+      console.log(dataMode);
       let dataFan = 0;
       switch (fan_speed) {
         case 0:
